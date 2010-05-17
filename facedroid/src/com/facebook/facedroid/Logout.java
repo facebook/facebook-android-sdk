87e6f3cde27428ad2324d2f774d7941324fcc041
Yariv Sadan <yariv@yariv2.local>
2010-05-16 20:44:28 -0700

    adding missing files

---
+++ facedroid/src/com/facebook/facedroid/Logout.java
@@ -0,0 +1,38 @@
+package com.facebook.facedroid;
+
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.android.Facebook;
+import com.facebook.android.Facebook.RequestListener;
+
+public class Logout extends Handler {
+
+	public void go() {
+		Facebook fb = SessionStore.getSession();
+		
+		// clear the local session data
+		SessionStore.clearSavedSession(getActivity());
+
+		// Asynchronously, ask the server to expire the access token.
+		// This is important to do in case the access token gets compromised somehow.
+		Bundle apiParams = new Bundle();
+		apiParams.putString("method", "auth.expireSession");
+		fb.request(apiParams, new RequestListener() {
+
+			@Override
+			public void onRequestSucceed(JSONObject response) {
+				Log.d("app", "auth.expireSession succeeded");
+			}
+
+			@Override
+			public void onRequestFail(String error) {
+				Log.d("app", "auth.expireSession failed");
+			}
+		});
+		dispatcher.runHandler("login");
+	}
+}
