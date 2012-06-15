befa55eecfecc005d88d2d01e36894ed0e6cebb5
Michael Marucheck <mmarucheck@fb.com>
2012-06-15 00:58:15 -0700

    Adds a trivial unit test to Android SDK
    Summary:
    Initial step to get unit tests going before we implement
    stuff so we can verify as we go.
    
    Test Plan:
    - cd facebook/tests
    - ant all clean debug install run-tests
    
    Revert Plan:
    safe to revert
    
    Reviewers: clang, jacl, joeb
    
    Reviewed By: jacl
    
    Differential Revision: https://phabricator.fb.com/D495804

---
+++ facebook/TestApp/src/com/facebook/sdk/StatusActivity.java
@@ -0,0 +1,15 @@
+package com.facebook.sdk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StatusActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
