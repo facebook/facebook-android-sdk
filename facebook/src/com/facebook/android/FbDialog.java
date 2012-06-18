4cbe4e2f348e09b0dd1decbc51e0899c3052d00b
Michael Marucheck <mmarucheck@fb.com>
2012-06-18 13:44:39 -0700

    Disable password saving in FbDialog
    Summary:
    By default, Android WebViews store passwords in a per-application
    sqlite database.  This changes the default for the Facebook SDK.
    
    Test Plan:
    - Build from command-line.
    - Uninstall Facebook app, run Hackbook, log in, adb shell, inspect
      webview database to verify no password was saved.
    
    Revert Plan: OK
    
    Reviewers: clang, jacl, vijaye
    
    Reviewed By: vijaye
    
    Differential Revision: https://phabricator.fb.com/D497389
    
    Task ID: 1108189

--- facebook/src/com/facebook/android/FbDialog.java
+++ facebook/src/com/facebook/android/FbDialog.java
@@ -121,6 +121,7 @@ public class FbDialog extends Dialog {
         mWebView.loadUrl(mUrl);
         mWebView.setLayoutParams(FILL);
         mWebView.setVisibility(View.INVISIBLE);
+        mWebView.getSettings().setSavePassword(false);
         
         webViewContainer.setPadding(margin, margin, margin, margin);
         webViewContainer.addView(mWebView);
