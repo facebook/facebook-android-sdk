cb390d231dcffc93d5d480d662f359e13293a670
Chris Banes <chris@senab.co.uk>
2012-07-04 15:11:55 +0100

    Fix Import

--- facebook/src/com/facebook/android/FbDialog.java
+++ facebook/src/com/facebook/android/FbDialog.java
@@ -25,7 +25,6 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
