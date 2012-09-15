f8208027d0640994d24b4cc1ba871845cc3b400f
Ming Li <mingfli@fb.com>
2012-09-14 17:23:01 -0700

    Clear token info upon logout.
    Summary: Bug fix for loginView, calling closeAndClearTokenInformation instead of just close when logging out.
    
    Test Plan: tested on emulator.
    
    Reviewers: mmarucheck, clang, karthiks
    
    Reviewed By: mmarucheck
    
    CC: msdkexp@
    
    Differential Revision: https://phabricator.fb.com/D575528
    
    Task ID: 1406845

--- facebook/src/com/facebook/LoginView.java
+++ facebook/src/com/facebook/LoginView.java
@@ -245,13 +245,13 @@ public class LoginView extends Button {
                            .setCancelable(true)
                            .setPositiveButton(logout, new DialogInterface.OnClickListener() {
                                public void onClick(DialogInterface dialog, int which) {
-                                   openSession.close();
+                                   openSession.closeAndClearTokenInformation();
                                }
                            })
                            .setNegativeButton(cancel, null);
                     builder.create().show();
                 } else {
-                    openSession.close();
+                    openSession.closeAndClearTokenInformation();
                 }
             } else {
                 if (context instanceof Activity) {
