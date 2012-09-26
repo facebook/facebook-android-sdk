b51467bafd52bae3cc77c5083206341eba693869
Ming Li <mingfli@fb.com>
2012-09-25 17:02:19 -0700

    Remove photo feature from Scrumptious.
    Summary: Remove photo feature from Scrumptious.
    
    Test Plan: verified on app.
    
    Reviewers: mmarucheck, clang, karthiks
    
    Reviewed By: mmarucheck
    
    CC: msdkexp@
    
    Differential Revision: https://phabricator.fb.com/D584366
    
    Task ID: 1761748

--- samples/Scrumptious/src/com/facebook/scrumptious/SelectionFragment.java
+++ samples/Scrumptious/src/com/facebook/scrumptious/SelectionFragment.java
@@ -108,7 +108,6 @@ public class SelectionFragment extends Fragment {
         listElements.add(new EatListElement(0));
         listElements.add(new LocationListElement(1));
         listElements.add(new PeopleListElement(2));
-        listElements.add(new PhotoListElement(3));
 
         listView.setAdapter(new ActionListAdapter(getActivity(), R.id.selection_list, listElements));
     }
@@ -392,109 +391,6 @@ public class SelectionFragment extends Fragment {
             if (selectedPlace != null) {
                 action.setPlace(selectedPlace);
             }
-        }
-    }
-
-    private class PhotoListElement extends BaseListElement {
-        private static final int CAMERA = 0;
-        private static final int GALLERY = 1;
-
-        private int activity = -1;
-        private Bitmap photo = null;
-
-        public PhotoListElement(int requestCode) {
-            super(getActivity().getResources().getDrawable(R.drawable.action_photo),
-                  getActivity().getResources().getString(R.string.action_photo),
-                  getActivity().getResources().getString(R.string.action_photo_default),
-                  requestCode);
-        }
-
-        @Override
-        protected View.OnClickListener getOnClickListener() {
-            return new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    showPhotoChoice();
-                }
-            };
-        }
-
-        @Override
-        protected void onActivityResult(Intent data) {
-            if (activity == CAMERA) {
-                photo = (Bitmap) data.getExtras().get("data");
-            } else if (activity == GALLERY) {
-                Uri imageUri = data.getData();
-                String [] filePath = { MediaStore.Images.Media.DATA };
-                Cursor cursor = getActivity().getContentResolver().query(imageUri, filePath, null, null, null);
-                if (cursor != null) {
-                    cursor.moveToFirst();
-                    int columnIndex = cursor.getColumnIndex(filePath[0]);
-                    String fileName = cursor.getString(columnIndex);
-                    cursor.close();
-                    photo = BitmapFactory.decodeFile(fileName);
-                }
-            }
-            if (photo != null) {
-                setText2(getResources().getString(R.string.action_photo_ready));
-            }
-            activity = -1;
-        }
-
-        @Override
-        protected void populateOGAction(OpenGraphAction action) {
-            if (photo != null) {
-                Request photoUpload = Request.newUploadPhotoRequest(Session.getActiveSession(), photo, null);
-                Response uploadResponse = photoUpload.execute();
-                String id = getIdFromResponseOrShowError(uploadResponse);
-                if (id != null) {
-                    Request photoRequest = Request.newGraphPathRequest(Session.getActiveSession(), id, null);
-                    Response photoResponse = photoRequest.execute();
-                    PhotoObjectResponse photoObjectResponse =
-                            photoResponse.getGraphObjectAs(PhotoObjectResponse.class);
-                    String source = photoObjectResponse.getSource();
-                    if (source != null) {
-                        JSONObject imageObject = new JSONObject();
-                        try {
-                            imageObject.put("url", source);
-                            action.setImage(Arrays.asList(new JSONObject[] {imageObject}));
-                        } catch (JSONException e) {
-                        }
-                    }
-                }
-            }
-        }
-
-        private void showPhotoChoice() {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            CharSequence camera = getResources().getString(R.string.action_photo_camera);
-            CharSequence gallery = getResources().getString(R.string.action_photo_gallery);
-            builder.setCancelable(true).
-                    setItems(new CharSequence[] {camera, gallery}, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            if (i == CAMERA) {
-                                startCameraActivity();
-                                activity = CAMERA;
-                            } else if (i == GALLERY) {
-                                startGalleryActivity();
-                                activity = GALLERY;
-                            }
-                        }
-                    });
-            builder.show();
-        }
-
-        private void startCameraActivity() {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(intent, getRequestCode());
-        }
-
-        private void startGalleryActivity() {
-            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.setType("image/*");
-            String selectPicture = getResources().getString(R.string.select_picture);
-            startActivityForResult(Intent.createChooser(intent, selectPicture), getRequestCode());
         }
     }
 
