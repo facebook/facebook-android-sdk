6c503145d028b18c8edec60dc5be07bf27b8a483
Ming Li <mingfli@fb.com>
2012-10-02 10:46:47 -0700

    Use LoginButton on the Scrumptious splash page.
    Summary: Use the LoginButton rather than a generic button, also changed the background color so it doesn't clash.
    
    Test Plan: tested on emulator and device.
    
    Reviewers: mmarucheck, clang, karthiks
    
    Reviewed By: mmarucheck
    
    Differential Revision: https://phabricator.fb.com/D589840
    
    Task ID: 1768466

--- samples/Scrumptious/src/com/facebook/scrumptious/SplashFragment.java
+++ samples/Scrumptious/src/com/facebook/scrumptious/SplashFragment.java
@@ -12,19 +12,6 @@ public class SplashFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.splash, container, false);
-        Button loginButton = (Button) view.findViewById(R.id.splash_login_button);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Session session = Session.getActiveSession();
-                if (session != null && !session.isOpened() && !session.getState().isClosed()) {
-                    session.openForRead(getActivity());
-                } else {
-                    Session.sessionOpen(getActivity());
-                }
-            }
-        });
         return view;
     }
 }
