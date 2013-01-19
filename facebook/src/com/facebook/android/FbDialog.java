6f878618a05db1e94d9bd3824fdf37190bf7858e
Chris Banes <chris@senab.co.uk>
2013-01-19 10:14:42 +0000

    Fix Facebook SDK so it can be packaged as a JAR

--- facebook/src/com/facebook/android/FbDialog.java
+++ facebook/src/com/facebook/android/FbDialog.java
@@ -22,17 +22,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.facebook.android.Facebook.DialogListener;
@@ -42,8 +41,8 @@ public class FbDialog extends Dialog {
 	static final int FB_BLUE = 0xFF6D84B4;
 	static final float[] DIMENSIONS_DIFF_LANDSCAPE = { 20, 60 };
 	static final float[] DIMENSIONS_DIFF_PORTRAIT = { 40, 60 };
-	static final FrameLayout.LayoutParams FILL = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
-			ViewGroup.LayoutParams.FILL_PARENT);
+	static final FrameLayout.LayoutParams FILL = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.FILL_PARENT,
+			FrameLayout.LayoutParams.FILL_PARENT);
 	static final int MARGIN = 4;
 	static final int PADDING = 2;
 	static final String DISPLAY_STRING = "touch";
@@ -52,12 +51,11 @@ public class FbDialog extends Dialog {
 	private String mUrl;
 	private DialogListener mListener;
 	private ProgressDialog mSpinner;
-	private ImageView mCrossImage;
 	private WebView mWebView;
 	private FrameLayout mContent;
 
 	public FbDialog(Context context, String url, DialogListener listener) {
-		super(context, android.R.style.Theme_Translucent_NoTitleBar);
+		super(context);
 		mUrl = url;
 		mListener = listener;
 	}
@@ -79,51 +77,22 @@ public class FbDialog extends Dialog {
 		mSpinner.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		mSpinner.setMessage("Loading...");
 
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setTitle("Facebook");
+
 		mContent = new FrameLayout(getContext());
 
-		/*
-		 * Create the 'x' image, but don't add to the mContent layout yet at
-		 * this point, we only need to know its drawable width and height to
-		 * place the webview
-		 */
-		createCrossImage();
+		setUpWebView();
 
 		/*
-		 * Now we know 'x' drawable width and height, layout the webivew and add
-		 * it the mContent layout
+		 * Finally add mContent to the Dialog view
 		 */
-		int crossWidth = mCrossImage.getDrawable().getIntrinsicWidth();
-		setUpWebView(crossWidth / 2);
+		setContentView(mContent, new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+				ViewGroup.LayoutParams.FILL_PARENT));
 
-		/*
-		 * Finally add the 'x' image to the mContent layout and add mContent to
-		 * the Dialog view
-		 */
-		mContent.addView(mCrossImage, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-		addContentView(mContent, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+		getWindow().setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.FILL_PARENT);
 	}
 
-	private void createCrossImage() {
-		mCrossImage = new ImageView(getContext());
-		// Dismiss the dialog when user click on the 'x'
-		mCrossImage.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				mListener.onCancel();
-				FbDialog.this.dismiss();
-			}
-		});
-		Drawable crossDrawable = getContext().getResources().getDrawable(R.drawable.close);
-		mCrossImage.setImageDrawable(crossDrawable);
-		/*
-		 * 'x' should not be visible while webview is loading make it visible
-		 * only after webview has fully loaded
-		 */
-		mCrossImage.setVisibility(View.INVISIBLE);
-	}
-
-	private void setUpWebView(int margin) {
+	private void setUpWebView() {
 		LinearLayout webViewContainer = new LinearLayout(getContext());
 		mWebView = new WorkaroundWebView(getContext());
 		mWebView.setVerticalScrollBarEnabled(false);
@@ -133,8 +102,6 @@ public class FbDialog extends Dialog {
 		mWebView.loadUrl(mUrl);
 		mWebView.setLayoutParams(FILL);
 		mWebView.setVisibility(View.INVISIBLE);
-
-		webViewContainer.setPadding(margin, margin, margin, margin);
 		webViewContainer.addView(mWebView);
 		mContent.addView(webViewContainer);
 	}
@@ -210,7 +177,6 @@ public class FbDialog extends Dialog {
 			 */
 			mContent.setBackgroundColor(Color.TRANSPARENT);
 			mWebView.setVisibility(View.VISIBLE);
-			mCrossImage.setVisibility(View.VISIBLE);
 		}
 	}
 
