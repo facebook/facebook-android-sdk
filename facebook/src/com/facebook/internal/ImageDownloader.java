38caa050749f3816196a6a5263c9e52f520a51f8
Cristian Oliveira <contato@cristianoliveira.com.br>
2014-10-18 18:15:15 -0300

    Android 2.1 - Fixed NullpointerException when request timeout while dowloading image.

--- facebook/src/com/facebook/internal/ImageDownloader.java
+++ facebook/src/com/facebook/internal/ImageDownloader.java
@@ -247,18 +247,21 @@ public class ImageDownloader {
                 default:
                     stream = connection.getErrorStream();
 
-                    // Android 2.1 - When timeout stream return null and cause NullpointerException
-	            if( stream != null ){
-		        InputStreamReader reader = new InputStreamReader(stream);
-		        char[] buffer = new char[128];
-		        int bufferLength;
-		        StringBuilder errorMessageBuilder = new StringBuilder();
-		        while ((bufferLength = reader.read(buffer, 0, buffer.length)) > 0) {
-		              errorMessageBuilder.append(buffer, 0, bufferLength);
-		        }
-		        Utility.closeQuietly(reader);
-		        error = new FacebookException(errorMessageBuilder.toString());
-	            }
+                    // Android 2.1 - When request timeout stream return null and cause NullPointerException
+    	            if (stream != null){
+                        InputStreamReader reader = new InputStreamReader(stream);
+    		            char[] buffer = new char[128];
+    		            int bufferLength;
+    		            StringBuilder errorMessageBuilder = new StringBuilder();
+    		            while ((bufferLength = reader.read(buffer, 0, buffer.length)) > 0) {
+    		                errorMessageBuilder.append(buffer, 0, bufferLength);
+    		            }
+    		            Utility.closeQuietly(reader);
+    		            error = new FacebookException(errorMessageBuilder.toString());
+    	            } else {
+                        error = new FacebookException(R.string.com_facebook_image_download_unknown_error);
+                    }
+
                     break;
             }
         } catch (IOException e) {
