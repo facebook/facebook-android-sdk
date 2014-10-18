70da1258bcbb29bedbba1d2bea88cb33e63745ba
Cristian Oliveira <contato@cristianoliveira.com.br>
2014-10-18 19:36:14 -0300

    Android 2.1 - Fixed NullpointerException when request timeout while downloading an image.

--- facebook/src/com/facebook/internal/ImageDownloader.java
+++ facebook/src/com/facebook/internal/ImageDownloader.java
@@ -246,21 +246,23 @@ public class ImageDownloader {
 
                 default:
                     stream = connection.getErrorStream();
-                    // Android 2.1-When request timeout stream return null and cause NullPointerException
+                    
+                    // Android 2.1 - When request timeout stream return null and cause NullPointerException
+                    StringBuilder errorMessageBuilder = new StringBuilder();
                     if (stream != null){
                         InputStreamReader reader = new InputStreamReader(stream);
                         char[] buffer = new char[128];
                         int bufferLength;
-                        StringBuilder errorMessageBuilder = new StringBuilder();
+                        
                         while ((bufferLength = reader.read(buffer, 0, buffer.length)) > 0) {
                             errorMessageBuilder.append(buffer, 0, bufferLength);
                         }
                         Utility.closeQuietly(reader);
-                        error = new FacebookException(errorMessageBuilder.toString());
                     } else {
-                        error = new FacebookException(R.string.com_facebook_image_download_unknown_error);
-                    
+                        errorMessageBuilder.append(R.string.com_facebook_image_download_unknown_error);
+                    }
 
+                    error = new FacebookException(errorMessageBuilder.toString());
                     break;
             }
         } catch (IOException e) {
