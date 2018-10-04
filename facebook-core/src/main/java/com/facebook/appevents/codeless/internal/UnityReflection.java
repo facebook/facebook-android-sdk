a255395b138e2aabb4eb91917a0829d112543e51
Wei Liu <weiliu@fb.com>
2018-10-03 19:50:20 -0700

    Add a helper class to call unity function from Android
    Summary: Call UnityPlayer.UnitySendMessage via Java reflection.
    
    Reviewed By: codytwinton
    
    Differential Revision: D10161286
    
    fbshipit-source-id: 27fbc4c66da18fa2b41739bdc854cf40079ebadf

---
+++ facebook-core/src/main/java/com/facebook/appevents/codeless/internal/UnityReflection.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+ * copy, modify, and distribute this software in source code or binary form for use
+ * in connection with the web services and APIs provided by Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use of
+ * this software is subject to the Facebook Developer Principles and Policies
+ * [http://developers.facebook.com/policy/]. This copyright notice shall be
+ * included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.facebook.appevents.codeless.internal;
+
+import android.util.Log;
+
+import java.lang.reflect.Method;
+
+public class UnityReflection {
+    private static final String TAG = UnityReflection.class.getCanonicalName();
+    private static final String UNITY_PLAYER_CLASS = "com.unity3d.player.UnityPlayer";
+    private static final String UNITY_SEND_MESSAGE_METHOD = "UnitySendMessage";
+    private static Class<?> unityPlayer;
+
+    public static void SendMessage(String unityObject, String unityMethod, String message) {
+        try {
+            if (unityPlayer == null) {
+                unityPlayer = Class.forName(UNITY_PLAYER_CLASS);
+            }
+
+            Method method = unityPlayer.getMethod(
+                    UNITY_SEND_MESSAGE_METHOD, String.class, String.class, String.class);
+            method.invoke(unityPlayer, unityObject, unityMethod, message);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to send message to Unity", e);
+        }
+    }
+}
