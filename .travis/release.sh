efa7eed645e574a1157e4e53dcfdbb14c1e8bfe4
Jing Ping <jingping@fb.com>
2019-02-22 11:03:14 -0800

    cleanup

--- .travis/release.sh
+++ .travis/release.sh
@@ -66,7 +66,6 @@ deploy_to_maven() {
   id1=`git log -n 1 --pretty=format:%H -- $VERSION_CLASS`
   id2=`git log -n 1 --pretty=format:%H`
   echo "start deploy......"
-  #if [ "$id1" == "$id2" ]; then
 
   FB_SRC_FOLDERS=(
     'facebook-core'
@@ -76,7 +75,7 @@ deploy_to_maven() {
     'facebook-places'
     'facebook-messenger'
     'facebook-applinks'
-    'facebook-marketing'
+    # 'facebook-marketing'
     'facebook'
   )
   for (( i = 0; i < ${#FB_SRC_FOLDERS[@]}; i++ ))
@@ -84,13 +83,10 @@ deploy_to_maven() {
     FOLDER=${FB_SRC_FOLDERS[$i]}
     echo "Publishing $FOLDER SDK to maven central";
     cp secring.gpg $FOLDER/
-    #./gradlew uploadArchives -p $FOLDER -PossrhUsername=${NEXUS_USERNAME} -PossrhPassword=${NEXUS_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=secring.gpg || die "Failed to publish $FOLDER SDK to maven central"
+    ./gradlew uploadArchives -p $FOLDER -PossrhUsername=${NEXUS_USERNAME} -PossrhPassword=${NEXUS_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=secring.gpg || die "Failed to publish $FOLDER SDK to maven central"
     rm $FOLDER/secring.gpg
   done
   rm secring.gpg
-  #else
-  #  echo 'No version update for this commit.';
-  #fi
 }
 
 # Proper Semantic Version
