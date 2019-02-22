88e4dab344f936312a41e1d67c220c958ddf0652
Jing Ping <jingping@fb.com>
2019-02-22 12:58:34 -0800

    Add shell check disable

--- .travis/release.sh
+++ .travis/release.sh
@@ -16,6 +16,12 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+# shellcheck disable=SC2148
+# shellcheck disable=SC2086
+# shellcheck disable=SC2154
+# shellcheck disable=SC2006
+# shellcheck disable=SC2034
+
 main() {
   VERSION_CLASS=facebook-core/src/main/java/com/facebook/FacebookSdkVersion.java
 
