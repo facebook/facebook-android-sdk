1f352fbb720657501feae64a771daac5f6ee5fcc
Nacho Quintero Rizo <roboto@fb.com>
2020-02-07 15:04:26 -0800

    Add GamingImageUploader (#679)
    Summary:
    Pull Request resolved: https://github.com/facebook/facebook-android-sdk/pull/679
    
    Adds GamingImageUploader a convencience wrapper around async image uploads to Gaming Media Library.
    
    Also  adds OpenGamingMediaDialog which is a callback handler for the raw GraphRequest.
    
    Reviewed By: joeymrios
    
    Differential Revision: D19758538
    
    fbshipit-source-id: 6ab2c2b82ec59f10ffd24255158faa378f36bfa1

---
+++ facebook-gamingservices/src/main/java/com/facebook/gamingservices/OpenGamingMediaDialog.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+ * copy, modify, and distribute this software in source code or binary form for use
+ * in connection with the web services and APIs provided by Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use of
+ * this software is subject to the Facebook Developer Principles and Policies
+ * [http://developers.facebook.com/policy/]. This copyright notice shall be
+ * included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.facebook.gamingservices;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+
+/**
+ *  Callback Handler to show the Gaming Media Dialog after media is
+ *  uploaded to the Gaming Media Library.
+ */
+public class OpenGamingMediaDialog implements GraphRequest.Callback {
+
+    private Context context;
+
+    public OpenGamingMediaDialog(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onCompleted(GraphResponse response) {
+        String id = response.getJSONObject().optString("id", null);
+        if (id != null) {
+            String dialog_uri = "https://fb.gg/me/media_asset/" + id;
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(dialog_uri));
+            this.context.startActivity(intent);
+        }
+    }
+}
