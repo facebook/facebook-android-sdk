43e8aa22e5972acac9dd342001f462e496184d3c
Callum Abele <callumabele@Callums-MacBook-Pro.local>
2020-02-20 08:32:01 +0000

    added exception handling and logging around referrerClient use

--- facebook-core/src/main/java/com/facebook/internal/InstallReferrerUtil.java
+++ facebook-core/src/main/java/com/facebook/internal/InstallReferrerUtil.java
@@ -2,7 +2,7 @@ package com.facebook.internal;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.RemoteException;
+import android.util.Log;
 
 import com.android.installreferrer.api.InstallReferrerClient;
 import com.android.installreferrer.api.InstallReferrerStateListener;
@@ -12,6 +12,8 @@ import com.facebook.FacebookSdk;
 import static com.facebook.FacebookSdk.APP_EVENT_PREFERENCES;
 
 public class InstallReferrerUtil {
+
+    private static final String TAG = InstallReferrerUtil.class.getSimpleName();
 
     private static final String IS_REFERRER_UPDATED = "is_referrer_updated";
 
@@ -24,38 +26,45 @@ public class InstallReferrerUtil {
     }
 
     private static void tryConnectReferrerInfo(final Callback callback) {
-        final InstallReferrerClient referrerClient = InstallReferrerClient.newBuilder(FacebookSdk.getApplicationContext()).build();
-        referrerClient.startConnection(new InstallReferrerStateListener() {
-            @Override
-            public void onInstallReferrerSetupFinished(int responseCode) {
-                switch (responseCode) {
-                    case InstallReferrerClient.InstallReferrerResponse.OK:
-                        ReferrerDetails response;
-                        try {
-                            response = referrerClient.getInstallReferrer();
-                        } catch (Exception e) {
-                            return;
-                        }
 
-                        String referrerUrl = response.getInstallReferrer();
-                        if (referrerUrl != null && (referrerUrl.contains("fb") || referrerUrl.contains("facebook"))) {
-                            callback.onReceiveReferrerUrl(referrerUrl);
-                        }
-                        updateReferrer(); //even if we are not interested in the url, there is no reason to ask for it again
-                        break;
+        try
+        {
+            final InstallReferrerClient referrerClient = InstallReferrerClient.newBuilder(FacebookSdk.getApplicationContext()).build();
+            referrerClient.startConnection(new InstallReferrerStateListener() {
+                @Override
+                public void onInstallReferrerSetupFinished(int responseCode) {
+                    switch (responseCode) {
+                        case InstallReferrerClient.InstallReferrerResponse.OK:
+                            ReferrerDetails response;
+                            try {
+                                response = referrerClient.getInstallReferrer();
+                            } catch (Exception e) {
+                                return;
+                            }
 
-                    case InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED:
-                        updateReferrer(); //No point retrying if feature not supported
-                        break;
-                    case InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE:
-                        break;
+                            String referrerUrl = response.getInstallReferrer();
+                            if (referrerUrl != null && (referrerUrl.contains("fb") || referrerUrl.contains("facebook"))) {
+                                callback.onReceiveReferrerUrl(referrerUrl);
+                            }
+                            updateReferrer(); //even if we are not interested in the url, there is no reason to ask for it again
+                            break;
+
+                        case InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED:
+                            updateReferrer(); //No point retrying if feature not supported
+                            break;
+                        case InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE:
+                            break;
+                    }
                 }
-            }
 
-            @Override
-            public void onInstallReferrerServiceDisconnected() {
-            }
-        });
+                @Override
+                public void onInstallReferrerServiceDisconnected() {
+                }
+            });
+        }
+        catch(Throwable ex) {
+            Log.e(TAG,"InstallReferrerClient thrown exception" + ex.getMessage());
+        }
     }
 
     private static void updateReferrer() {
