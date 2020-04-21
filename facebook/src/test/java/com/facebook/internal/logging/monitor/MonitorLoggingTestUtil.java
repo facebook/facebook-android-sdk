98982f5c1789619a4a7667d9cf0f76446ac7a0bb
Xinzhu Cai <kongxinzhu@fb.com>
2020-04-20 21:24:22 -0700

    Add Monitor class (#757)
    Summary:
    Pull Request resolved: https://github.com/facebook/facebook-android-sdk/pull/757
    
    Added Monitor class
    Monitor class is the entry point of the Monitoring System.
    The usage of the Monitoring System will be:
    ```
    // assign value of timeStart, timeSpent or the value want to be logged
    LogEvent logEvent = new LogEvent("FB_CORE_STARTUP", LogCategory.PERFORMANCE);
    MonitorLog log = new MonitorLog.LogBuilder(logEvent)
                    .timeStart(timeStart)
                    .timeSpent(timeSpent)
                    .build();
    Monitor.add(log);
    ```
    Monitor needs to be enabled before using it to add log. Enable Monitor will fetch the sampling rates from FB server, send the logs in the disk and clean the disk. *Enable part will be added in the next diff.*
    
    **Sampling rate** is an integer indicating that we will send one every x logs for a specific event name. We have a default sampling rate for all events which are not specified in the list.
    
    Reviewed By: Mxiim
    
    Differential Revision: D20903128
    
    fbshipit-source-id: d8db9c5428ec4b8680d6e59bfb90d1c3969132c6

--- facebook/src/test/java/com/facebook/internal/logging/monitor/MonitorLoggingTestUtil.java
+++ facebook/src/test/java/com/facebook/internal/logging/monitor/MonitorLoggingTestUtil.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ * <p>
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+ * copy, modify, and distribute this software in source code or binary form for use
+ * in connection with the web services and APIs provided by Facebook.
+ * <p>
+ * As with any software that integrates with the Facebook platform, your use of
+ * this software is subject to the Facebook Developer Principles and Policies
+ * [http://developers.facebook.com/policy/]. This copyright notice shall be
+ * included in all copies or substantial portions of the software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.facebook.internal.logging.monitor;
 
 import com.facebook.internal.logging.LogCategory;
@@ -15,6 +34,8 @@ public class MonitorLoggingTestUtil {
     static final int INVALID_TIME = -1;
     static final long INVALID_TIME_LONG = -1;
     static final LogEvent TEST_LOG_EVENT = new LogEvent(TEST_EVENT_NAME, TEST_CATEGORY);
+    static final int TEST_SAMPLING_RATE = 100;
+    static final Integer TEST_DEFAULT_SAMPLING_RATE = 20;
 
     public static MonitorLog getTestMonitorLog(long timeStart, int timeSpent) {
         MonitorLog log = new MonitorLog.LogBuilder(TEST_LOG_EVENT)
