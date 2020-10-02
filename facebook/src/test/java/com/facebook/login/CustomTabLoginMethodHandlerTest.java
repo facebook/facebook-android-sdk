3460baf35b345f58e0a509bb315facbaeb2c4989
generatedunixname89002005325674 <generatedunixname89002005325674@fb.com>
2020-10-02 03:39:48 -0700

    Daily `arc lint --take GOOGLEJAVAFORMAT`
    Reviewed By: zertosh
    
    Differential Revision: D24075844
    
    fbshipit-source-id: df939f1885dd646f3b561e217ce0e2c27626ff47

--- facebook/src/test/java/com/facebook/login/CustomTabLoginMethodHandlerTest.java
+++ facebook/src/test/java/com/facebook/login/CustomTabLoginMethodHandlerTest.java
@@ -21,10 +21,8 @@
 package com.facebook.login;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
