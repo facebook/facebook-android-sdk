a2c440a543fef675fa5ebc7ce5e43a776b1a6403
Jay Tang <jaytang@fb.com>
2020-11-09 19:34:22 -0800

    add new test cases for SmartLoginOption class
    Reviewed By: Mxiim
    
    Differential Revision: D24844361
    
    fbshipit-source-id: b77baeb798e0b884bc497d1b4bc3e8e2b74761b2

---
+++ facebook/src/test/kotlin/com/facebook/internal/SmartLoginOptionTest.kt
@@ -0,0 +1,17 @@
+package com.facebook.internal
+
+import java.util.*
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+import org.junit.Test
+
+class SmartLoginOptionTest {
+
+  @Test
+  fun `test SmartLoginOption parseOptions method`() {
+    assertEquals(SmartLoginOption.parseOptions(0), EnumSet.noneOf(SmartLoginOption::class.java))
+    assertEquals(SmartLoginOption.parseOptions(1), EnumSet.of(SmartLoginOption.Enabled))
+    assertEquals(SmartLoginOption.parseOptions(2), EnumSet.of(SmartLoginOption.RequireConfirm))
+    assertNotEquals(SmartLoginOption.parseOptions(3), EnumSet.of(SmartLoginOption.Enabled))
+  }
+}
