9c8cfbc24838086464387704efcde516a14f74ee
generatedunixname89002005325672 <generatedunixname89002005325672@fb.com>
2020-11-19 04:42:45 -0800

    Daily `arc lint --take KTFMT`
    Reviewed By: zertosh, cgrushko
    
    Differential Revision: D25085009
    
    fbshipit-source-id: 2f32caa66778cd7703cd7c8977cfcf561d91cb5d

--- facebook/src/test/kotlin/com/facebook/appevents/ml/MTensorTest.kt
+++ facebook/src/test/kotlin/com/facebook/appevents/ml/MTensorTest.kt
@@ -13,37 +13,69 @@ class MTensorTest : FacebookPowerMockTestCase() {
   fun `test reshape() and getData`() {
     // Capacity: 24 (2 * 3 * 4)
     val tensor = MTensor(intArrayOf(2, 3, 4))
-    val data = floatArrayOf(
-            1f, 3f, 5f, 7f,
-            5f, 7f, 9f, 11f,
-            9f, 11f, 13f, 15f,
-            13f, 15f, 17f, 19f,
-            17f, 19f, 21f, 23f,
-            21f, 23f, 25f, 27f
-    )
+    val data =
+        floatArrayOf(
+            1f,
+            3f,
+            5f,
+            7f,
+            5f,
+            7f,
+            9f,
+            11f,
+            9f,
+            11f,
+            13f,
+            15f,
+            13f,
+            15f,
+            17f,
+            19f,
+            17f,
+            19f,
+            21f,
+            23f,
+            21f,
+            23f,
+            25f,
+            27f)
     Whitebox.setInternalState(tensor, "data", data)
 
     // Reshape to capacity of 16 (2 * 2 * 4)
     tensor.reshape(intArrayOf(2, 2, 4))
-    var expectedData = floatArrayOf(
-            1f, 3f, 5f, 7f,
-            5f, 7f, 9f, 11f,
-            9f, 11f, 13f, 15f,
-            13f, 15f, 17f, 19f
-    )
+    var expectedData =
+        floatArrayOf(1f, 3f, 5f, 7f, 5f, 7f, 9f, 11f, 9f, 11f, 13f, 15f, 13f, 15f, 17f, 19f)
     assertArrayEquals(tensor.data, expectedData, 0.0001f)
     assertEquals(Whitebox.getInternalState<Any>(tensor, "capacity"), 16)
 
     // Reshape to capacity of 24 (2 * 3 * 4)
     tensor.reshape(intArrayOf(2, 3, 4))
-    expectedData = floatArrayOf(
-            1f, 3f, 5f, 7f,
-            5f, 7f, 9f, 11f,
-            9f, 11f, 13f, 15f,
-            13f, 15f, 17f, 19f,
-            0f, 0f, 0f, 0f,
-            0f, 0f, 0f, 0f
-    )
+    expectedData =
+        floatArrayOf(
+            1f,
+            3f,
+            5f,
+            7f,
+            5f,
+            7f,
+            9f,
+            11f,
+            9f,
+            11f,
+            13f,
+            15f,
+            13f,
+            15f,
+            17f,
+            19f,
+            0f,
+            0f,
+            0f,
+            0f,
+            0f,
+            0f,
+            0f,
+            0f)
     assertArrayEquals(tensor.data, expectedData, 0.0001f)
     assertEquals(Whitebox.getInternalState<Any>(tensor, "capacity"), 24)
   }
