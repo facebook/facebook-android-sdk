f3c3c20811b04550caa00a345be36adcc916c21b
Mengxiao Lin <mxlin@fb.com>
2021-04-15 10:49:20 -0700

    Migrate FacebookInitProvider to Kotlin
    Summary: As title.
    
    Reviewed By: jingping2015
    
    Differential Revision: D27757190
    
    fbshipit-source-id: 64f4e4184f0620576caf4cf4a038801c4b29438f

---
+++ facebook-core/src/main/java/com/facebook/internal/FacebookInitProvider.kt
@@ -0,0 +1,56 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+package com.facebook.internal
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+import android.util.Log
+import com.facebook.FacebookSdk
+import java.lang.Exception
+
+class FacebookInitProvider : ContentProvider() {
+  companion object {
+    private val TAG = FacebookInitProvider::class.java.simpleName
+  }
+  @SuppressWarnings("deprecation")
+  override fun onCreate(): Boolean {
+    try {
+      FacebookSdk.sdkInitialize(context)
+    } catch (ex: Exception) {
+      Log.i(TAG, "Failed to auto initialize the Facebook SDK", ex)
+    }
+    return false
+  }
+
+  override fun query(
+      uri: Uri,
+      projection: Array<String>?,
+      selection: String?,
+      selectionArgs: Array<String>?,
+      sortOrder: String?
+  ): Cursor? {
+    return null
+  }
+
+  override fun getType(uri: Uri): String? {
+    return null
+  }
+
+  override fun insert(uri: Uri, values: ContentValues?): Uri? {
+    return null
+  }
+
+  override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+    return 0
+  }
+
+  override fun update(
+      uri: Uri,
+      values: ContentValues?,
+      selection: String?,
+      selectionArgs: Array<String>?
+  ): Int {
+    return 0
+  }
+}
