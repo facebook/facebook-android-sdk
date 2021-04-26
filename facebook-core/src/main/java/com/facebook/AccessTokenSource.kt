1b1815216bb127b8df61080f3071516b9d449777
Mengxiao Lin <mxlin@fb.com>
2021-04-26 09:53:00 -0700

    Migrate AccessTokenSource into Kotlin
    Summary: As title.
    
    Reviewed By: KylinChang
    
    Differential Revision: D27971866
    
    fbshipit-source-id: c5bee6c2a768717775bda6243e61d17b5cacfaff

--- facebook-core/src/main/java/com/facebook/AccessTokenSource.kt
+++ facebook-core/src/main/java/com/facebook/AccessTokenSource.kt
@@ -18,49 +18,51 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package com.facebook;
+package com.facebook
 
 /** Indicates where a Facebook access token was obtained from. */
-public enum AccessTokenSource {
+enum class AccessTokenSource(private val canExtendToken: Boolean) {
   /** Indicates an access token has not been obtained, or is otherwise invalid. */
   NONE(false),
+
   /**
    * Indicates an access token was obtained by the user logging in through the Facebook app for
    * Android using the web login dialog.
    */
   FACEBOOK_APPLICATION_WEB(true),
+
   /**
    * Indicates an access token was obtained by the user logging in through the Facebook app for
    * Android using the native login dialog.
    */
   FACEBOOK_APPLICATION_NATIVE(true),
+
   /**
    * Indicates an access token was obtained by asking the Facebook app for the current token based
    * on permissions the user has already granted to the app. No dialog was shown to the user in this
    * case.
    */
   FACEBOOK_APPLICATION_SERVICE(true),
+
   /** Indicates an access token was obtained by the user logging in through the Web-based dialog. */
   WEB_VIEW(true),
+
   /**
    * Indicates an access token was obtained by the user logging in through the Web-based dialog on a
    * Chrome Custom Tab.
    */
   CHROME_CUSTOM_TAB(true),
+
   /** Indicates an access token is for a test user rather than an actual Facebook user. */
   TEST_USER(true),
+
   /** Indicates an access token constructed with a Client Token. */
   CLIENT_TOKEN(true),
+
   /** Indicates an access token constructed from facebook.com/device */
   DEVICE_AUTH(true);
 
-  private final boolean canExtendToken;
-
-  AccessTokenSource(boolean canExtendToken) {
-    this.canExtendToken = canExtendToken;
-  }
-
-  boolean canExtendToken() {
-    return canExtendToken;
+  fun canExtendToken(): Boolean {
+    return canExtendToken
   }
 }
