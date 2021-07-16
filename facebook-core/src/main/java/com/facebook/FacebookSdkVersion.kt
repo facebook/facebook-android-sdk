2c23e2688f44d163d56d251f15e5b0f7eacc6410
Jing Ping <jingping@fb.com>
2021-07-16 13:53:25 -0700

    Migrate FacebookSdkVersion to Kotlin
    Summary: As title
    
    Reviewed By: linmx0130
    
    Differential Revision: D29724377
    
    fbshipit-source-id: 97daf5fa0123acfa4b1eb2bfb0ea2e5f3d548f7b

--- facebook-core/src/main/java/com/facebook/FacebookSdkVersion.kt
+++ facebook-core/src/main/java/com/facebook/FacebookSdkVersion.kt
@@ -17,9 +17,8 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+package com.facebook
 
-package com.facebook;
-
-final class FacebookSdkVersion {
-  public static final String BUILD = "11.1.0";
+internal object FacebookSdkVersion {
+  const val BUILD = "11.1.0"
 }
