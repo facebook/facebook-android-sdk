ff96eb5c54769aba963a79a6e3acf4465e305211
Mengxiao Lin <mxlin@fb.com>
2021-07-23 12:38:58 -0700

    Refactor instrument tests for Kotlin 1.5
    Summary: Powermock's `thenCallRealMethod` will fail on Kotlin 1.5. By refactoring instrument tests to remove these usage, we can smoothly migrate to new Kotlin JVM/IR backend.
    
    Reviewed By: jingping2015
    
    Differential Revision: D29882104
    
    fbshipit-source-id: a00db12010a59a0340ab6d1beff68a2fa22a8377

--- facebook/src/test/kotlin/com/facebook/internal/instrument/anrreport/ANRHandlerTest.kt
+++ facebook/src/test/kotlin/com/facebook/internal/instrument/anrreport/ANRHandlerTest.kt
@@ -3,23 +3,22 @@ package com.facebook.internal.instrument.anrreport
 import androidx.test.core.app.ApplicationProvider
 import com.facebook.FacebookPowerMockTestCase
 import com.facebook.FacebookSdk
-import com.facebook.internal.instrument.InstrumentData
+import com.facebook.GraphRequest
 import com.facebook.internal.instrument.InstrumentUtility
-import com.facebook.util.common.anyObject
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.anyOrNull
 import java.io.File
-import java.util.*
+import java.util.UUID
 import org.json.JSONArray
 import org.json.JSONObject
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
-import org.mockito.ArgumentMatchers
 import org.powermock.api.mockito.PowerMockito
 import org.powermock.core.classloader.annotations.PrepareForTest
 
-@PrepareForTest(
-    ANRDetector::class, InstrumentUtility::class, InstrumentData.Builder::class, FacebookSdk::class)
+@PrepareForTest(ANRDetector::class, InstrumentUtility::class, FacebookSdk::class)
 class ANRHandlerTest : FacebookPowerMockTestCase() {
   private lateinit var root: File
   private lateinit var directory: File
@@ -37,21 +36,8 @@ class ANRHandlerTest : FacebookPowerMockTestCase() {
     PowerMockito.`when`(FacebookSdk.isInitialized()).thenReturn(true)
     PowerMockito.`when`(FacebookSdk.getApplicationContext())
         .thenReturn(ApplicationProvider.getApplicationContext())
-    PowerMockito.mockStatic(InstrumentData.Builder::class.java)
-
-    PowerMockito.mockStatic(InstrumentUtility::class.java)
-    PowerMockito.`when`(InstrumentUtility.getInstrumentReportDir()).thenReturn(directory)
-    PowerMockito.`when`(InstrumentUtility.listAnrReportFiles()).thenCallRealMethod()
-    PowerMockito.`when`(
-            InstrumentUtility.writeFile(
-                ArgumentMatchers.isA(String::class.java), ArgumentMatchers.isA(String::class.java)))
-        .thenCallRealMethod()
-    PowerMockito.`when`(
-            InstrumentUtility.readFile(
-                ArgumentMatchers.isA(String::class.java),
-                ArgumentMatchers.isA(Boolean::class.java)))
-        .thenCallRealMethod()
-    PowerMockito.`when`(InstrumentData.Builder.load(anyObject())).thenCallRealMethod()
+    PowerMockito.spy(InstrumentUtility::class.java)
+    PowerMockito.doReturn(directory).`when`(InstrumentUtility::class.java, "getInstrumentReportDir")
   }
 
   @After
@@ -64,12 +50,16 @@ class ANRHandlerTest : FacebookPowerMockTestCase() {
     var hitSendReports = 0
     var hitStartANRDetection = 0
     PowerMockito.`when`(FacebookSdk.getAutoLogAppEventsEnabled()).thenReturn(true)
-
-    PowerMockito.`when`(InstrumentUtility.sendReports(anyObject(), anyObject(), anyObject()))
-        .thenAnswer {
+    PowerMockito.doAnswer {
           hitSendReports += 1
           null
         }
+        .`when`(
+            InstrumentUtility::class.java,
+            "sendReports",
+            anyOrNull<String>(),
+            any<JSONArray>(),
+            anyOrNull<GraphRequest.Callback>())
     PowerMockito.`when`(ANRDetector.start()).then {
       hitStartANRDetection += 1
       return@then Unit
@@ -86,12 +76,16 @@ class ANRHandlerTest : FacebookPowerMockTestCase() {
   fun `test not to send report if app events disabled or data processing restricted`() {
     var hitSendReports = 0
     PowerMockito.`when`(FacebookSdk.getAutoLogAppEventsEnabled()).thenReturn(false)
-
-    PowerMockito.`when`(InstrumentUtility.sendReports(anyObject(), anyObject(), anyObject()))
-        .thenAnswer {
+    PowerMockito.doAnswer {
           hitSendReports += 1
           null
         }
+        .`when`(
+            InstrumentUtility::class.java,
+            "sendReports",
+            anyOrNull<String>(),
+            any<JSONArray>(),
+            anyOrNull<GraphRequest.Callback>())
 
     ANRHandler.enable()
     ANRHandler.enable()
@@ -110,11 +104,17 @@ class ANRHandlerTest : FacebookPowerMockTestCase() {
     InstrumentUtility.writeFile(
         "anr_log_3.json", "{\"reason\":\"yoloswag\",\"callstack\":[],\"timestamp\":3}")
     var anrLogs: JSONArray? = null
-    PowerMockito.`when`(InstrumentUtility.sendReports(anyObject(), anyObject(), anyObject()))
-        .thenAnswer {
+    PowerMockito.doAnswer {
           anrLogs = it.arguments[1] as JSONArray
           null
         }
+        .`when`(
+            InstrumentUtility::class.java,
+            "sendReports",
+            anyOrNull<String>(),
+            any<JSONArray>(),
+            anyOrNull<GraphRequest.Callback>())
+
     ANRHandler.sendANRReports()
     // check the order of the reports is correct
     val anrLogsTimeStamps =
