5ff57913d310e6461f2386e7efe5ae2b3bec1cf2
Mengxiao Lin <mxlin@fb.com>
2021-08-06 14:14:16 -0700

    Add test for ViewIndexingTrigger
    Summary: As title
    
    Reviewed By: jingping2015
    
    Differential Revision: D30166364
    
    fbshipit-source-id: 97c735ba45df009d0115a25b851b4043cb01f1bc

--- facebook-core/src/main/java/com/facebook/appevents/codeless/ViewIndexingTrigger.java
+++ facebook-core/src/main/java/com/facebook/appevents/codeless/ViewIndexingTrigger.java
@@ -25,10 +25,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import com.facebook.internal.instrument.crashshield.AutoHandleExceptions;
-import com.facebook.internal.qualityvalidation.Excuse;
-import com.facebook.internal.qualityvalidation.ExcusesForDesignViolations;
 
-@ExcusesForDesignViolations(@Excuse(type = "MISSING_UNIT_TEST", reason = "Legacy"))
 @AutoHandleExceptions
 class ViewIndexingTrigger implements SensorEventListener {
 
