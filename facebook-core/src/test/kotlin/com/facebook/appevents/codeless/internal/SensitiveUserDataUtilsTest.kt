f8cf57d8203305798c518efddcfba63ed2510c92
Jing Ping <jingping@fb.com>
2021-08-12 21:17:06 -0700

    Move more test to Kotlin
    Summary: As title
    
    Reviewed By: linmx0130
    
    Differential Revision: D30287734
    
    fbshipit-source-id: 8d33bc856579061f803642fe926806692c95a525

---
+++ facebook-core/src/test/kotlin/com/facebook/appevents/codeless/internal/SensitiveUserDataUtilsTest.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+ * copy, modify, and distribute this software in source code or binary form for use
+ * in connection with the web services and APIs provided by Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use of
+ * this software is subject to the Facebook Developer Principles and Policies
+ * [http://developers.facebook.com/policy/]. This copyright notice shall be
+ * included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.facebook.appevents.codeless.internal
+
+import android.text.InputType
+import android.widget.TextView
+import com.facebook.appevents.codeless.CodelessTestBase
+import com.facebook.appevents.codeless.internal.SensitiveUserDataUtils.isSensitiveUserData
+import com.nhaarman.mockitokotlin2.mock
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+import org.powermock.api.mockito.PowerMockito
+
+class SensitiveUserDataUtilsTest : CodelessTestBase() {
+  private lateinit var textView: TextView
+
+  @Before
+  override fun setup() {
+    super.setup()
+    textView = mock()
+  }
+
+  @Test
+  fun testIsSensitiveUserData() {
+    PowerMockito.`when`(textView.text).thenReturn("")
+
+    // input type == Password
+    PowerMockito.`when`(textView.inputType).thenReturn(InputType.TYPE_TEXT_VARIATION_PASSWORD)
+    Assert.assertTrue(isSensitiveUserData(textView))
+
+    // input type == Text
+    PowerMockito.`when`(textView.inputType).thenReturn(InputType.TYPE_CLASS_TEXT)
+    Assert.assertFalse(isSensitiveUserData(textView))
+
+    // input type == Person Name
+    PowerMockito.`when`(textView.inputType).thenReturn(InputType.TYPE_TEXT_VARIATION_PERSON_NAME)
+    Assert.assertTrue(isSensitiveUserData(textView))
+
+    // input type == Postal Address
+    PowerMockito.`when`(textView.inputType).thenReturn(InputType.TYPE_TEXT_VARIATION_POSTAL_ADDRESS)
+    Assert.assertTrue(isSensitiveUserData(textView))
+
+    // input type == Phone
+    Mockito.`when`(textView.inputType).thenReturn(InputType.TYPE_CLASS_PHONE)
+    Assert.assertTrue(isSensitiveUserData(textView))
+
+    // Credit Card
+    PowerMockito.`when`(textView.inputType).thenReturn(InputType.TYPE_CLASS_TEXT)
+    PowerMockito.`when`(textView.text).thenReturn("4030122707427751")
+    Assert.assertTrue(isSensitiveUserData(textView))
+  }
+}
