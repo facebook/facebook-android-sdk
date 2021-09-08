86ba775b20170c3f027290fd8c88899edff199de
Jing Ping <jingping@fb.com>
2021-09-07 23:53:48 -0700

    1/n Move tests to common kits
    Reviewed By: jawwad
    
    Differential Revision: D30683321
    
    fbshipit-source-id: a0d06939990dc41c86a657b07de75d4c74719a51

---
+++ facebook-common/src/test/kotlin/com/facebook/login/AuthenticationTokenTestUtil.kt
@@ -0,0 +1,60 @@
+package com.facebook.login
+
+import android.text.format.DateUtils
+import com.facebook.AuthenticationToken
+import com.facebook.AuthenticationTokenClaims
+
+object AuthenticationTokenTestUtil {
+
+  const val VALID_HEADER_STRING = "eyJhbGciOiJSUzI1NiIsInR5cCI6Imp3dCIsImtpZCI6ImFiYyJ9"
+  const val JTI = "jti"
+  const val APP_ID = "123456789" // aud
+  const val ISS = "https://facebook.com/dialog/oauth"
+  const val SUB = "1234"
+  const val NONCE = "nonce"
+  val IAT = System.currentTimeMillis() / 1000
+  val EXPIRATION_DATE =
+      (System.currentTimeMillis() + DateUtils.HOUR_IN_MILLIS) / 1000 // valid only within 60 minutes
+
+  @JvmField
+  val AUTH_TOKEN_CLAIMS_FOR_TEST =
+      AuthenticationTokenClaims(
+          JTI,
+          ISS,
+          APP_ID,
+          NONCE,
+          EXPIRATION_DATE, // exp
+          IAT, // iat
+          SUB,
+          "name",
+          "givenName",
+          "middleName",
+          "familyName",
+          "email",
+          "picture",
+          listOf("friend1", "friend2"),
+          "userBirthday",
+          hashMapOf("min" to 20),
+          hashMapOf("id" to "112724962075996", "name" to "Martinez, California"),
+          hashMapOf("id" to "110843418940484", "name" to "Seattle, Washington"),
+          "male",
+          "facebook.com")
+
+  @JvmField
+  val authenticationTokenClaimsWithRequiredFieldsOnly =
+      AuthenticationTokenClaims(JTI, ISS, APP_ID, NONCE, EXPIRATION_DATE, IAT, SUB)
+
+  @JvmStatic
+  fun getAuthenticationTokenForTest(): AuthenticationToken {
+    return AuthenticationToken(getEncodedAuthTokenStringForTest(), NONCE)
+  }
+
+  @JvmStatic
+  fun getEncodedAuthTokenStringForTest(): String {
+    val sb = StringBuilder()
+    sb.append(VALID_HEADER_STRING).append(".")
+    sb.append(AUTH_TOKEN_CLAIMS_FOR_TEST.toEnCodedString()).append(".")
+    sb.append("Signature")
+    return sb.toString()
+  }
+}
