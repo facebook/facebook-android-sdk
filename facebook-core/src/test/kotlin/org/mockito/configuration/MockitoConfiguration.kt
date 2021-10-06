fd0ec0f838a4a84ce473cba06d6851d09261ee8d
Jing Ping <jingping@fb.com>
2021-10-06 10:40:15 -0700

    Make core-kit pure Kotlin
    Summary:
    Since our core SDK already pure Kotlin, make sense to set this flag be True
    check related post here https://fb.workplace.com/groups/androidfyi/permalink/5794010843980816/
    
    After this, I think we can remove `JavaOnlyKotlinValidationRule` https://fburl.com/code/3dutdi8x since this flag should enforce only Kotlin file be added in core module.
    
    Reviewed By: linmx0130
    
    Differential Revision: D31414317
    
    fbshipit-source-id: fedc1c74b50e7aa4ffd2763f03632633b31eb0e6

--- facebook-core/src/test/kotlin/org/mockito/configuration/MockitoConfiguration.kt
+++ facebook-core/src/test/kotlin/org/mockito/configuration/MockitoConfiguration.kt
@@ -17,13 +17,10 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+package org.mockito.configuration
 
-package org.mockito.configuration;
-
-public class MockitoConfiguration extends DefaultMockitoConfiguration {
-
-  @Override
-  public boolean enableClassCache() {
-    return false;
+class MockitoConfiguration : DefaultMockitoConfiguration() {
+  override fun enableClassCache(): Boolean {
+    return false
   }
 }
