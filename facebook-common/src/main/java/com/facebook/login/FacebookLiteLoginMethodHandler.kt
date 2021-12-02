e9ef0bc6ed1c7bdfdd907bbf7b650f6ce797e9c5
CiCi Cai <cicicai@fb.com>
2021-12-02 11:20:09 -0800

    Migrate FacebookLiteLoginMethodHandler to Kotlin
    Summary: As title ^
    
    Reviewed By: linmx0130
    
    Differential Revision: D32710941
    
    fbshipit-source-id: 937bad885cad1f1ccc1175fca7b1cb18eac3e188

---
+++ facebook-common/src/main/java/com/facebook/login/FacebookLiteLoginMethodHandler.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+ * copy, modify, and distribute this software in source code or binary form for use
+ * in connection with the web services and APIs provided by Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use of
+ * this software is subject to the Facebook Developer Principles and Policies
+ * [http://developers.facebook.com/policy/]. This copyright notice shall be
+ * included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.facebook.login
+
+import android.os.Parcel
+import android.os.Parcelable
+import com.facebook.internal.NativeProtocol
+import com.facebook.internal.ServerProtocol
+
+internal class FacebookLiteLoginMethodHandler : NativeAppLoginMethodHandler {
+
+  constructor(loginClient: LoginClient) : super(loginClient)
+  override val nameForLogging = "fb_lite_login"
+
+  override fun tryAuthorize(request: LoginClient.Request): Int {
+    val e2e = LoginClient.getE2E()
+    val intent =
+        NativeProtocol.createFacebookLiteIntent(
+            loginClient.activity,
+            request.applicationId,
+            request.permissions,
+            e2e,
+            request.isRerequest,
+            request.hasPublishPermission(),
+            request.defaultAudience,
+            getClientState(request.authId),
+            request.authType,
+            request.messengerPageId,
+            request.resetMessengerState,
+            request.isFamilyLogin,
+            request.shouldSkipAccountDeduplication())
+
+    addLoggingExtra(ServerProtocol.DIALOG_PARAM_E2E, e2e)
+
+    val result = tryIntent(intent, LoginClient.getLoginRequestCode())
+    return if (result) 1 else 0
+  }
+
+  constructor(source: Parcel) : super(source)
+
+  override fun describeContents(): Int {
+    return 0
+  }
+
+  override fun writeToParcel(dest: Parcel, flags: Int) {
+    super.writeToParcel(dest, flags)
+  }
+
+  companion object {
+    @JvmField
+    val CREATOR =
+        object : Parcelable.Creator<FacebookLiteLoginMethodHandler> {
+          override fun createFromParcel(source: Parcel): FacebookLiteLoginMethodHandler {
+            return FacebookLiteLoginMethodHandler(source)
+          }
+
+          override fun newArray(size: Int): Array<FacebookLiteLoginMethodHandler?> {
+            return arrayOfNulls(size)
+          }
+        }
+  }
+}
