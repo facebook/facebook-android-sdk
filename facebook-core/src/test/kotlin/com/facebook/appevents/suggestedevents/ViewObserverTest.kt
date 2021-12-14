d3efafbb50e4eea90b35cc2dfb5c695b372bb141
Mengxiao Lin <mxlin@fb.com>
2021-12-13 22:32:01 -0800

    Refactor ViewObserverTest to avoid non-necessary mock
    Summary: As title. By doing so the unit test of `ViewObserver` can avoid bytecode modification on the class to be tested.
    
    Reviewed By: jawwad
    
    Differential Revision: D33051481
    
    fbshipit-source-id: 6fac8c733f3cd8e80123f8bfc97dc75143b3839f

--- facebook-core/src/test/kotlin/com/facebook/appevents/suggestedevents/ViewObserverTest.kt
+++ facebook-core/src/test/kotlin/com/facebook/appevents/suggestedevents/ViewObserverTest.kt
@@ -9,7 +9,6 @@ import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
-import java.lang.ref.WeakReference
 import java.util.concurrent.atomic.AtomicBoolean
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
@@ -18,20 +17,17 @@ import org.powermock.api.mockito.PowerMockito
 import org.powermock.core.classloader.annotations.PrepareForTest
 import org.powermock.reflect.Whitebox
 
-@PrepareForTest(ViewObserver::class, AppEventUtility::class)
+@PrepareForTest(AppEventUtility::class)
 class ViewObserverTest : FacebookPowerMockTestCase() {
   private lateinit var mockActivity: Activity
-  private lateinit var mockObserver: ViewObserver
   private lateinit var mockView: View
   private lateinit var mockViewTreeObserver: ViewTreeObserver
+
   @Before
   fun init() {
     mockActivity = mock()
-    mockObserver = mock()
     mockView = mock()
     mockViewTreeObserver = mock()
-    PowerMockito.whenNew(ViewObserver::class.java).withAnyArguments().thenReturn(mockObserver)
-    Whitebox.setInternalState(mockObserver, "activityWeakReference", WeakReference(mockActivity))
 
     PowerMockito.mockStatic(AppEventUtility::class.java)
     whenever(AppEventUtility.getRootView(any())).thenReturn(mockView)
@@ -41,34 +37,30 @@ class ViewObserverTest : FacebookPowerMockTestCase() {
 
   @Test
   fun testStartTrackingActivity() {
-    Whitebox.setInternalState(mockObserver, "isTracking", AtomicBoolean(false))
     ViewObserver.startTrackingActivity(mockActivity)
     val observers =
-        Whitebox.getInternalState<Map<Integer, ViewObserver>>(ViewObserver::class.java, "observers")
+        Whitebox.getInternalState<Map<Int, ViewObserver>>(ViewObserver::class.java, "observers")
+    checkNotNull(observers)
     assertThat(observers).isNotEmpty
-    assertThat(observers).isEqualTo(hashMapOf(mockActivity.hashCode() as Integer to mockObserver))
-
-    val isTracking = Whitebox.getInternalState<AtomicBoolean>(mockObserver, "isTracking")
+    val observer: ViewObserver? = observers[mockActivity.hashCode()]
+    val isTracking = Whitebox.getInternalState<AtomicBoolean>(observer, "isTracking")
     assertThat(isTracking.get()).isTrue
-
     verify(mockViewTreeObserver).addOnGlobalLayoutListener(any())
   }
 
   @Test
   fun testStopTrackingActivity() {
-    Whitebox.setInternalState(mockObserver, "isTracking", AtomicBoolean(true))
-    Whitebox.setInternalState(
-        ViewObserver::class.java,
-        "observers",
-        hashMapOf(mockActivity.hashCode() as Integer to mockObserver))
-    ViewObserver.stopTrackingActivity(mockActivity)
+    ViewObserver.startTrackingActivity(mockActivity)
     val observers =
-        Whitebox.getInternalState<Map<Integer, ViewObserver>>(ViewObserver::class.java, "observers")
+        Whitebox.getInternalState<Map<Int, ViewObserver>>(ViewObserver::class.java, "observers")
+    checkNotNull(observers)
+    val observer: ViewObserver? = observers[mockActivity.hashCode()]
+
+    ViewObserver.stopTrackingActivity(mockActivity)
+
     assertThat(observers).isEmpty()
-
-    val isTracking = Whitebox.getInternalState<AtomicBoolean>(mockObserver, "isTracking")
+    val isTracking = Whitebox.getInternalState<AtomicBoolean>(observer, "isTracking")
     assertThat(isTracking.get()).isFalse
-
     verify(mockViewTreeObserver).removeOnGlobalLayoutListener(any())
   }
 }
