2fcdd2ba3be764c9483f3e0001ecfbee49d1acc4
Mengxiao Lin <mxlin@fb.com>
2022-02-24 12:52:47 -0800

    Backfill unit tests for SharePhoto
    Summary: As title.
    
    Reviewed By: jawwad
    
    Differential Revision: D34454338
    
    fbshipit-source-id: 6e63cddf6d4f6e930b9b26dab042a1110b4c53e2

--- facebook-common/src/main/java/com/facebook/share/model/SharePhoto.java
+++ facebook-common/src/main/java/com/facebook/share/model/SharePhoto.java
@@ -25,8 +25,6 @@ import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
 import androidx.annotation.Nullable;
-import com.facebook.internal.qualityvalidation.Excuse;
-import com.facebook.internal.qualityvalidation.ExcusesForDesignViolations;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,7 +33,6 @@ import java.util.List;
  *
  * <p>Use {@link SharePhoto.Builder} to build instances
  */
-@ExcusesForDesignViolations(@Excuse(type = "MISSING_UNIT_TEST", reason = "Legacy"))
 public final class SharePhoto extends ShareMedia {
   private final Bitmap bitmap;
   private final Uri imageUrl;
---
+++ facebook-common/src/test/kotlin/com/facebook/share/model/SharePhotoTest.kt
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+ * copy, modify, and distribute this software in source code or binary form for use
+ * in connection with the web services and APIs provided by Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use of
+ * this software is subject to the Facebook Developer Principles and Policies
+ * [http://developers.facebook.com/policy/]. This copyright notice shall be
+ * included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.facebook.share.model
+
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Parcel
+import com.facebook.FacebookTestCase
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class SharePhotoTest : FacebookTestCase() {
+  private lateinit var testBitmap: Bitmap
+  private lateinit var testImageUri: Uri
+  private lateinit var testSharePhoto: SharePhoto
+  private val testCaption = "test caption"
+
+  override fun setUp() {
+    super.setUp()
+    testBitmap = Bitmap.createBitmap(1, 2, Bitmap.Config.ALPHA_8)
+    testImageUri = Uri.parse("https://facebook.com/1.png")
+    testSharePhoto =
+        SharePhoto.Builder()
+            .setBitmap(testBitmap)
+            .setImageUrl(testImageUri)
+            .setCaption(testCaption)
+            .setUserGenerated(true)
+            .build()
+  }
+
+  @Test
+  fun `test creating a photo with bitmap`() {
+    val sharePhoto =
+        SharePhoto.Builder()
+            .setBitmap(testBitmap)
+            .setCaption(testCaption)
+            .setUserGenerated(true)
+            .build()
+
+    assertThat(sharePhoto.imageUrl).isNull()
+    assertThat(sharePhoto.bitmap).isNotNull
+    assertThat(sharePhoto.bitmap?.width).isEqualTo(1)
+    assertThat(sharePhoto.bitmap?.height).isEqualTo(2)
+    assertThat(sharePhoto.bitmap?.config).isEqualTo(Bitmap.Config.ALPHA_8)
+    assertThat(sharePhoto.caption).isEqualTo(testCaption)
+    assertThat(sharePhoto.userGenerated).isTrue
+  }
+
+  @Test
+  fun `test creating a photo with uri`() {
+    val sharePhoto =
+        SharePhoto.Builder()
+            .setImageUrl(testImageUri)
+            .setCaption(testCaption)
+            .setUserGenerated(false)
+            .build()
+
+    assertThat(sharePhoto.imageUrl?.toString()).isEqualTo(testImageUri.toString())
+    assertThat(sharePhoto.bitmap).isNull()
+    assertThat(sharePhoto.caption).isEqualTo(testCaption)
+    assertThat(sharePhoto.userGenerated).isFalse
+  }
+
+  @Test
+  fun `test creating share photo from an existing share photo`() {
+    val sharePhoto = SharePhoto.Builder().readFrom(testSharePhoto).build()
+
+    assertThat(sharePhoto.imageUrl.toString()).isEqualTo(testImageUri.toString())
+    assertThat(sharePhoto.bitmap).isNotNull
+    assertThat(sharePhoto.bitmap?.width).isEqualTo(1)
+    assertThat(sharePhoto.bitmap?.height).isEqualTo(2)
+    assertThat(sharePhoto.bitmap?.config).isEqualTo(Bitmap.Config.ALPHA_8)
+    assertThat(sharePhoto.caption).isEqualTo(testCaption)
+    assertThat(sharePhoto.userGenerated).isTrue
+  }
+
+  @Test
+  fun `test recovering share photo from parcel`() {
+    val parcel = Parcel.obtain()
+    parcel.writeParcelable(testSharePhoto, 0)
+    parcel.setDataPosition(0)
+
+    val sharePhoto = parcel.readParcelable<SharePhoto>(SharePhoto::class.java.classLoader)
+
+    checkNotNull(sharePhoto)
+    assertThat(sharePhoto.imageUrl.toString()).isEqualTo(testImageUri.toString())
+    assertThat(sharePhoto.bitmap).isNotNull
+    assertThat(sharePhoto.bitmap?.width).isEqualTo(1)
+    assertThat(sharePhoto.bitmap?.height).isEqualTo(2)
+    assertThat(sharePhoto.bitmap?.config).isEqualTo(Bitmap.Config.ALPHA_8)
+    assertThat(sharePhoto.caption).isEqualTo(testCaption)
+    assertThat(sharePhoto.userGenerated).isTrue
+
+    parcel.recycle()
+  }
+}
