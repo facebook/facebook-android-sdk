147029e81e4d4a37a6e4e62d932f72896842683d
Mengxiao Lin <mxlin@fb.com>
2022-03-03 10:39:54 -0800

    Backfill unit tests for NativeAppCallAttachmentStore
    Summary: As title. Added the test for handling file attachment, `cleanupAttachmentsForCall` and `openAttachment`. Also to create new call id for each test run and switch to use assertj.
    
    Differential Revision: D34586843
    
    fbshipit-source-id: 9e6198fbf95791dc88ff0a60d9100e4b944af67b

--- facebook-core/src/test/kotlin/com/facebook/internal/NativeAppCallAttachmentStoreTest.kt
+++ facebook-core/src/test/kotlin/com/facebook/internal/NativeAppCallAttachmentStoreTest.kt
@@ -27,9 +27,11 @@ import com.facebook.FacebookException
 import com.facebook.FacebookPowerMockTestCase
 import com.facebook.FacebookSdk
 import com.nhaarman.mockitokotlin2.whenever
+import java.io.File
+import java.io.FileNotFoundException
 import java.util.UUID
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
-import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.powermock.api.mockito.PowerMockito
@@ -37,8 +39,10 @@ import org.powermock.core.classloader.annotations.PrepareForTest
 
 @PrepareForTest(FacebookSdk::class)
 class NativeAppCallAttachmentStoreTest : FacebookPowerMockTestCase() {
+  private lateinit var callId: UUID
+  private lateinit var testDirectory: File
+
   companion object {
-    private val CALL_ID = UUID.randomUUID()
     private const val ATTACHMENT_NAME = "hello"
     private const val UNEXPECTED_ATTACHMENT_URI = "test://nothissiteintheworld.test/abcd"
     private const val CONTENT_URI = "content://testcontent.test/1234"
@@ -52,81 +56,127 @@ class NativeAppCallAttachmentStoreTest : FacebookPowerMockTestCase() {
     whenever(FacebookSdk.getApplicationContext())
         .thenReturn(ApplicationProvider.getApplicationContext())
     NativeAppCallAttachmentStore.ensureAttachmentsDirectoryExists()
+
+    callId = UUID.randomUUID()
+    testDirectory = File(UUID.randomUUID().toString())
+    testDirectory.mkdir()
   }
 
   @After
   fun clean() {
     NativeAppCallAttachmentStore.cleanupAllAttachments()
+    testDirectory.deleteRecursively()
   }
 
   @Test
   fun `test add attachments of bitmap`() {
-    val attachment = NativeAppCallAttachmentStore.createAttachment(CALL_ID, createBitmap())
+    val attachment = NativeAppCallAttachmentStore.createAttachment(callId, createBitmap())
     val attachmentUrl = attachment.attachmentUrl
-    Assert.assertNotNull(attachmentUrl)
+    assertThat(attachmentUrl).isNotNull
     NativeAppCallAttachmentStore.addAttachments(listOf(attachment))
 
     // Url format: base/call_id/filename
     val attachmentFilename = attachmentUrl.split("/").last()
     val retrievedAttachment =
-        NativeAppCallAttachmentStore.getAttachmentFile(CALL_ID, attachmentFilename, false)
-    Assert.assertNotNull(retrievedAttachment)
+        NativeAppCallAttachmentStore.getAttachmentFile(callId, attachmentFilename, false)
     checkNotNull(retrievedAttachment)
-    Assert.assertTrue(retrievedAttachment.exists())
+    assertThat(retrievedAttachment.exists()).isTrue
   }
 
-  @Test
+  @Test(expected = FacebookException::class)
   fun `test create attachments with unexpected uri`() {
-    try {
-      NativeAppCallAttachmentStore.createAttachment(CALL_ID, Uri.parse(UNEXPECTED_ATTACHMENT_URI))
-      Assert.fail("expected exception")
-    } catch (e: FacebookException) {
-      Assert.assertTrue(e.message?.contains("Unsupported scheme") ?: false)
-    }
+    NativeAppCallAttachmentStore.createAttachment(callId, Uri.parse(UNEXPECTED_ATTACHMENT_URI))
   }
 
   @Test
   fun `test create attachments with content uri`() {
-    val attachment = NativeAppCallAttachmentStore.createAttachment(CALL_ID, Uri.parse(CONTENT_URI))
-    Assert.assertNotNull(attachment)
-    checkNotNull(attachment)
-    Assert.assertEquals(attachment.originalUri.toString(), CONTENT_URI)
+    val attachment = NativeAppCallAttachmentStore.createAttachment(callId, Uri.parse(CONTENT_URI))
+    assertThat(attachment.originalUri.toString()).isEqualTo(CONTENT_URI)
   }
 
   @Test
   fun `test create attachments with media uri`() {
-    val attachment = NativeAppCallAttachmentStore.createAttachment(CALL_ID, Uri.parse(MEDIA_URI))
-    Assert.assertNotNull(attachment)
-    checkNotNull(attachment)
-    Assert.assertEquals(attachment.originalUri.toString(), MEDIA_URI)
-    Assert.assertEquals(attachment.attachmentUrl, MEDIA_URI)
+    val attachment = NativeAppCallAttachmentStore.createAttachment(callId, Uri.parse(MEDIA_URI))
+    assertThat(attachment.originalUri.toString()).isEqualTo(MEDIA_URI)
+    assertThat(attachment.attachmentUrl).isEqualTo(MEDIA_URI)
   }
 
   @Test
   fun `test get attachments directory`() {
     val dir = NativeAppCallAttachmentStore.getAttachmentsDirectory()
-    Assert.assertNotNull(dir)
     checkNotNull(dir)
-    Assert.assertTrue(dir.absolutePath.contains(NativeAppCallAttachmentStore.ATTACHMENTS_DIR_NAME))
+    assertThat(dir.canonicalPath).contains(NativeAppCallAttachmentStore.ATTACHMENTS_DIR_NAME)
   }
 
   @Test
   fun `test get attachments directory for call`() {
-    val dir = NativeAppCallAttachmentStore.getAttachmentsDirectoryForCall(CALL_ID, false)
-    Assert.assertNotNull(dir)
+    val dir = NativeAppCallAttachmentStore.getAttachmentsDirectoryForCall(callId, false)
     checkNotNull(dir)
-    Assert.assertTrue(dir.absolutePath.contains(NativeAppCallAttachmentStore.ATTACHMENTS_DIR_NAME))
-    Assert.assertTrue(dir.absolutePath.contains(CALL_ID.toString()))
+    assertThat(dir.canonicalPath).contains(NativeAppCallAttachmentStore.ATTACHMENTS_DIR_NAME)
+    assertThat(dir.canonicalPath).contains(callId.toString())
   }
 
   @Test
   fun `test get attachment file`() {
-    val dir = NativeAppCallAttachmentStore.getAttachmentFile(CALL_ID, ATTACHMENT_NAME, false)
-    Assert.assertNotNull(dir)
+    val dir = NativeAppCallAttachmentStore.getAttachmentFile(callId, ATTACHMENT_NAME, false)
     checkNotNull(dir)
-    Assert.assertTrue(dir.absolutePath.contains(NativeAppCallAttachmentStore.ATTACHMENTS_DIR_NAME))
-    Assert.assertTrue(dir.absolutePath.contains(CALL_ID.toString()))
-    Assert.assertTrue(dir.absolutePath.contains(ATTACHMENT_NAME))
+    assertThat(dir.canonicalPath).contains(NativeAppCallAttachmentStore.ATTACHMENTS_DIR_NAME)
+    assertThat(dir.canonicalPath).contains(callId.toString())
+    assertThat(dir.canonicalPath).contains(ATTACHMENT_NAME)
+  }
+
+  @Test
+  fun `test adding empty and null attachment list won't crash`() {
+    NativeAppCallAttachmentStore.addAttachments(null)
+    NativeAppCallAttachmentStore.addAttachments(listOf())
+  }
+
+  @Test
+  fun `test adding an attachment from url file`() {
+    // create attachment input file
+    val inputFile = File(testDirectory.path + "/input")
+    with(inputFile.outputStream()) {
+      this.write(7)
+      this.close()
+    }
+    val attachment = NativeAppCallAttachmentStore.createAttachment(callId, Uri.fromFile(inputFile))
+
+    // add attachment to the store
+    NativeAppCallAttachmentStore.addAttachments(listOf(attachment))
+    val retrievedAttachmentFile =
+        NativeAppCallAttachmentStore.openAttachment(callId, attachment.attachmentName)
+    checkNotNull(retrievedAttachmentFile)
+    with(retrievedAttachmentFile.inputStream()) {
+      val data = this.read()
+      assertThat(data).isEqualTo(7)
+    }
+  }
+
+  @Test(expected = FacebookException::class)
+  fun `test adding an attachment from a non-exist file will throw an exception`() {
+    val inputFile = File(testDirectory.path, "input")
+    val attachment = NativeAppCallAttachmentStore.createAttachment(callId, Uri.fromFile(inputFile))
+
+    NativeAppCallAttachmentStore.addAttachments(listOf(attachment))
+  }
+
+  @Test(expected = FileNotFoundException::class)
+  fun `test open an attachment of the empty name will throw an exception`() {
+    NativeAppCallAttachmentStore.openAttachment(callId, "")
+  }
+
+  @Test
+  fun `test cleaning up attachment will delete added attachments`() {
+    val attachment = NativeAppCallAttachmentStore.createAttachment(callId, createBitmap())
+    NativeAppCallAttachmentStore.addAttachments(listOf(attachment))
+    checkNotNull(
+        NativeAppCallAttachmentStore.getAttachmentFile(callId, attachment.attachmentName, false))
+
+    NativeAppCallAttachmentStore.cleanupAttachmentsForCall(callId)
+
+    val retrievedAttachmentFile =
+        NativeAppCallAttachmentStore.getAttachmentFile(callId, attachment.attachmentName, false)
+    assertThat(retrievedAttachmentFile?.exists()).isFalse
   }
 
   private fun createBitmap(): Bitmap {
