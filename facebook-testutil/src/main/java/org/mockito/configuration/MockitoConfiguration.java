8e564b831e066324e15435e75bed4004eb0074a1
Mengxiao Lin <mxlin@fb.com>
2022-04-18 17:17:25 -0700

    Move MockitoConfiguration to test util kit
    Summary: `MockitoConfiguration` is necessary for all kits to correctly make mocks. Based on this fact, it should be put in test util kit.
    
    Reviewed By: samodom
    
    Differential Revision: D35654114
    
    fbshipit-source-id: 775f1283aa6206f2dcab94bb18ee2961a5a4d3db

--- facebook-testutil/src/main/java/org/mockito/configuration/MockitoConfiguration.java
+++ facebook-testutil/src/main/java/org/mockito/configuration/MockitoConfiguration.java
@@ -21,7 +21,6 @@
 package org.mockito.configuration;
 
 public class MockitoConfiguration extends DefaultMockitoConfiguration {
-
   @Override
   public boolean enableClassCache() {
     return false;
