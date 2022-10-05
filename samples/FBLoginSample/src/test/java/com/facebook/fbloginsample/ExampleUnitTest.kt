9b204ed959c281a4a9e14c213641d200004ea783
Jawwad Ahmad <jawwad@fb.com>
2022-10-05 09:34:41 -0700

    Convert ExampleUnitTest.java to Kotlin
    Reviewed By: joesus
    
    Differential Revision: D40075626
    
    fbshipit-source-id: 6fa00839f17104632cea78a2039b6891562be630

--- samples/FBLoginSample/src/test/java/com/facebook/fbloginsample/ExampleUnitTest.kt
+++ samples/FBLoginSample/src/test/java/com/facebook/fbloginsample/ExampleUnitTest.kt
@@ -6,20 +6,19 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.fbloginsample;
+package com.facebook.fbloginsample
 
-import static junit.framework.Assert.assertEquals;
-
-import org.junit.Test;
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * See [testing documentation](http://d.android.com/tools/testing).
  */
-public class ExampleUnitTest {
+class ExampleUnitTest {
   @Test
-  public void addition_isCorrect() throws Exception {
-    assertEquals(4, 2 + 2);
+  fun addition_isCorrect() {
+    assertEquals(4, 2 + 2)
   }
 }
