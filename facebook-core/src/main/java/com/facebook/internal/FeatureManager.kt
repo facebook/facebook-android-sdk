4a5bfcea2c332c1a850b9396754fe32227bfdcda
Saranya Vidyananthan <saranyavidy@fb.com>
2022-10-19 11:41:43 -0700

    Adding Meta IAP and Meta Payments Features
    Reviewed By: OmarB97
    
    Differential Revision: D40512271
    
    fbshipit-source-id: 03d8b5dc1a7c470aa2da770e0dbf40a9e8a8ca8d

--- facebook-core/src/main/java/com/facebook/internal/FeatureManager.kt
+++ facebook-core/src/main/java/com/facebook/internal/FeatureManager.kt
@@ -126,6 +126,8 @@ object FeatureManager {
       Feature.OnDevicePostInstallEventProcessing,
       Feature.IapLogging,
       Feature.IapLoggingLib2,
+      Feature.MetaIAP,
+      Feature.MetaPayments,
       Feature.ChromeCustomTabsPrefetching,
       Feature.Monitoring,
       Feature.IgnoreAppSwitchToLoggedOut,
@@ -160,6 +162,8 @@ object FeatureManager {
     OnDevicePostInstallEventProcessing(0x00010601),
     IapLogging(0x00010700),
     IapLoggingLib2(0x00010701),
+    MetaIAP(0x00010702),
+    MetaPayments(0x00010800),
     Instrument(0x00020000),
     CrashReport(0x00020100),
     CrashShield(0x00020101),
@@ -203,6 +207,8 @@ object FeatureManager {
           OnDevicePostInstallEventProcessing -> "OnDevicePostInstallEventProcessing"
           IapLogging -> "IAPLogging"
           IapLoggingLib2 -> "IAPLoggingLib2"
+          MetaIAP -> "MetaIAP"
+          MetaPayments -> "MetaPayments"
           Monitoring -> "Monitoring"
           ServiceUpdateCompliance -> "ServiceUpdateCompliance"
           Login -> "LoginKit"
