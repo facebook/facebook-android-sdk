a1c35dfbb8379625e1ba5678a4d8b1a24554c124
Max Albright <maxalbright@meta.com>
2024-08-30 14:22:02 -0700

    Dynamically fetch all GPBL methods in wrapper
    Summary: The changes made in the code include dynamically fetching all Google Play Billing Library (GPBL) methods in the wrapper class to support v5-v7.
    
    Reviewed By: jjiang10
    
    Differential Revision: D60789910
    
    fbshipit-source-id: 634c5c27e6c78bc536bf05136ecff04a89018da7

--- facebook-core/src/main/java/com/facebook/appevents/iap/InAppPurchaseBillingClientWrapperV5Plus.kt
+++ facebook-core/src/main/java/com/facebook/appevents/iap/InAppPurchaseBillingClientWrapperV5Plus.kt
@@ -35,18 +35,47 @@ private constructor(
     private val productDetailsClazz: Class<*>,
     private val purchaseHistoryRecordClazz: Class<*>,
     private val queryProductDetailsParamsProductClazz: Class<*>,
+
     private val queryProductDetailsParamsClazz: Class<*>,
     private val queryPurchaseHistoryParamsClazz: Class<*>,
     private val queryPurchasesParamsClazz: Class<*>,
+
     private val queryProductDetailsParamsBuilderClazz: Class<*>,
     private val queryPurchaseHistoryParamsBuilderClazz: Class<*>,
     private val queryPurchasesParamsBuilderClazz: Class<*>,
     private val queryProductDetailsParamsProductBuilderClazz: Class<*>,
     private val billingClientBuilderClazz: Class<*>,
+
     private val purchasesUpdatedListenerClazz: Class<*>,
     private val billingClientStateListenerClazz: Class<*>,
     private val productDetailsResponseListenerClazz: Class<*>,
     private val purchasesResponseListenerClazz: Class<*>,
+    private val purchaseHistoryResponseListenerClazz: Class<*>,
+
+    private val queryPurchasesAsyncMethod: Method,
+    private val queryPurchasesParamsNewBuilderMethod: Method,
+    private val queryPurchasesParamsBuilderBuildMethod: Method,
+    private val queryPurchasesParamsBuilderSetProductTypeMethod: Method,
+
+    private val queryPurchaseHistoryAsyncMethod: Method,
+    private val queryPurchaseHistoryParamsNewBuilderMethod: Method,
+    private val queryPurchaseHistoryParamsBuilderBuildMethod: Method,
+    private val queryPurchaseHistoryParamsBuilderSetProductTypeMethod: Method,
+
+    private val queryProductDetailsAsyncMethod: Method,
+    private val queryProductDetailsParamsNewBuilderMethod: Method,
+    private val queryProductDetailsParamsBuilderBuildMethod: Method,
+    private val queryProductDetailsParamsBuilderSetProductListMethod: Method,
+    private val queryProductDetailsParamsProductNewBuilderMethod: Method,
+    private val queryProductDetailsParamsProductBuilderBuildMethod: Method,
+    private val queryProductDetailsParamsProductBuilderSetProductIdMethod: Method,
+    private val queryProductDetailsParamsProductBuilderSetProductTypeMethod: Method,
+
+    private val billingClientNewBuilderMethod: Method,
+    private val billingClientBuilderSetListenerMethod: Method,
+    private val billingClientBuilderEnablePendingPurchasesMethod: Method,
+    private val billingClientBuilderBuildMethod: Method,
+    private val billingClientStartConnectionMethod: Method
 ) {
 
     companion object {
@@ -98,6 +127,26 @@ private constructor(
         private const val CLASSNAME_PURCHASES_RESPONSE_LISTENER =
             "com.android.billingclient.api.PurchasesResponseListener"
 
+        // Method names
+        private const val METHOD_SET_PRODUCT_ID = "setProductId"
+        private const val METHOD_SET_PRODUCT_TYPE = "setProductType"
+        private const val METHOD_SET_PRODUCT_LIST = "setProductList"
+        private const val METHOD_QUERY_PURCHASES_ASYNC = "queryPurchasesAsync"
+        private const val METHOD_QUERY_PRODUCT_DETAILS_ASYNC = "queryProductDetailsAsync"
+        private const val METHOD_QUERY_PURCHASE_HISTORY_ASYNC = "queryPurchaseHistoryAsync"
+        private const val METHOD_NEW_BUILDER = "newBuilder"
+        private const val METHOD_BUILD = "build"
+
+        private const val METHOD_ENABLE_PENDING_PURCHASES = "enablePendingPurchases"
+        private const val METHOD_SET_LISTENER = "setListener"
+        private const val METHOD_START_CONNECTION = "startConnection"
+
+        // Method names: Listeners
+        private const val METHOD_ON_BILLING_SETUP_FINISHED = "onBillingSetupFinished"
+        private const val METHOD_ON_BILLING_SERVICE_DISCONNECTED = "onBillingServiceDisconnected"
+        private const val METHOD_ON_PURCHASE_HISTORY_RESPONSE = "onPurchaseHistoryResponse"
+        private const val METHOD_ON_QUERY_PURCHASES_RESPONSE = "onQueryPurchasesResponse"
+        private const val METHOD_ON_PRODUCT_DETAILS_RESPONSE = "onProductDetailsResponse"
 
         @Synchronized
         @JvmStatic
@@ -145,24 +194,31 @@ private constructor(
                 CLASSNAME_PRODUCT_DETAILS_RESPONSE_LISTENER
             )
             val purchasesResponseListenerClazz = getClass(CLASSNAME_PURCHASES_RESPONSE_LISTENER)
+            val purchaseHistoryResponseListenerClazz = getClass(
+                CLASSNAME_PURCHASE_HISTORY_RESPONSE_LISTENER
+            )
 
             if (billingClientClazz == null ||
                 purchaseClazz == null ||
                 productDetailsClazz == null ||
                 purchaseHistoryRecordClazz == null ||
                 queryProductDetailsParamsProductClazz == null ||
+
                 queryProductDetailsParamsClazz == null ||
                 queryPurchaseHistoryParamsClazz == null ||
                 queryPurchasesParamsClazz == null ||
+
                 queryProductDetailsParamsBuilderClazz == null ||
                 queryPurchaseHistoryParamsBuilderClazz == null ||
                 queryPurchasesParamsBuilderClazz == null ||
                 queryProductDetailsParamsProductBuilderClazz == null ||
                 billingClientBuilderClazz == null ||
+
                 purchasesUpdatedListenerClazz == null ||
                 billingClientStateListenerClazz == null ||
                 productDetailsResponseListenerClazz == null ||
-                purchasesResponseListenerClazz == null
+                purchasesResponseListenerClazz == null ||
+                purchaseHistoryResponseListenerClazz == null
 
             ) {
                 Log.w(
@@ -172,7 +228,129 @@ private constructor(
                 return
             }
 
-            // TODO: Get methods
+            // Get methods: Query purchases
+            val queryPurchasesAsyncMethod =
+                getMethod(
+                    billingClientClazz,
+                    METHOD_QUERY_PURCHASES_ASYNC,
+                    queryPurchasesParamsClazz,
+                    purchasesResponseListenerClazz
+                )
+            val queryPurchasesParamsNewBuilderMethod =
+                getMethod(queryPurchasesParamsClazz, METHOD_NEW_BUILDER)
+            val queryPurchasesParamsBuilderBuildMethod =
+                getMethod(queryPurchasesParamsBuilderClazz, METHOD_BUILD)
+            val queryPurchasesParamsBuilderSetProductTypeMethod =
+                getMethod(
+                    queryPurchasesParamsBuilderClazz,
+                    METHOD_SET_PRODUCT_TYPE,
+                    String::class.java
+                )
+
+            // Get methods: Query purchase history
+            val queryPurchaseHistoryAsyncMethod = getMethod(
+                billingClientClazz,
+                METHOD_QUERY_PURCHASE_HISTORY_ASYNC,
+                queryPurchaseHistoryParamsClazz,
+                purchaseHistoryResponseListenerClazz
+            )
+            val queryPurchaseHistoryParamsNewBuilderMethod =
+                getMethod(queryPurchaseHistoryParamsClazz, METHOD_NEW_BUILDER)
+            val queryPurchaseHistoryParamsBuilderBuildMethod =
+                getMethod(queryPurchaseHistoryParamsBuilderClazz, METHOD_BUILD)
+            val queryPurchaseHistoryParamsBuilderSetProductTypeMethod =
+                getMethod(
+                    queryPurchaseHistoryParamsBuilderClazz,
+                    METHOD_SET_PRODUCT_TYPE,
+                    String::class.java
+                )
+
+            // Get methods: Query product details
+            val queryProductDetailsAsyncMethod =
+                getMethod(
+                    billingClientClazz,
+                    METHOD_QUERY_PRODUCT_DETAILS_ASYNC,
+                    queryProductDetailsParamsClazz,
+                    productDetailsResponseListenerClazz
+                )
+            val queryProductDetailsParamsNewBuilderMethod =
+                getMethod(queryProductDetailsParamsClazz, METHOD_NEW_BUILDER)
+            val queryProductDetailsParamsBuilderBuildMethod =
+                getMethod(queryProductDetailsParamsBuilderClazz, METHOD_BUILD)
+            val queryProductDetailsParamsBuilderSetProductListMethod =
+                getMethod(
+                    queryProductDetailsParamsBuilderClazz,
+                    METHOD_SET_PRODUCT_LIST,
+                    List::class.java
+                )
+            val queryProductDetailsParamsProductNewBuilderMethod =
+                getMethod(queryProductDetailsParamsProductClazz, METHOD_NEW_BUILDER)
+            val queryProductDetailsParamsProductBuilderBuildMethod =
+                getMethod(queryProductDetailsParamsProductBuilderClazz, METHOD_BUILD)
+            val queryProductDetailsParamsProductBuilderSetProductIdMethod =
+                getMethod(
+                    queryProductDetailsParamsProductBuilderClazz,
+                    METHOD_SET_PRODUCT_ID,
+                    String::class.java
+                )
+            val queryProductDetailsParamsProductBuilderSetProductTypeMethod =
+                getMethod(
+                    queryProductDetailsParamsProductBuilderClazz,
+                    METHOD_SET_PRODUCT_TYPE,
+                    String::class.java
+                )
+
+            // Get methods: Create billing client
+            val billingClientNewBuilderMethod =
+                getMethod(billingClientClazz, METHOD_NEW_BUILDER, Context::class.java)
+            val billingClientBuilderSetListenerMethod =
+                getMethod(
+                    billingClientBuilderClazz,
+                    METHOD_SET_LISTENER,
+                    purchasesUpdatedListenerClazz
+                )
+            val billingClientBuilderEnablePendingPurchasesMethod =
+                getMethod(billingClientBuilderClazz, METHOD_ENABLE_PENDING_PURCHASES)
+            val billingClientBuilderBuildMethod = getMethod(billingClientBuilderClazz, METHOD_BUILD)
+            val billingClientStartConnectionMethod =
+                getMethod(
+                    billingClientClazz,
+                    METHOD_START_CONNECTION,
+                    billingClientStateListenerClazz
+                )
+
+            if (
+                queryPurchasesAsyncMethod == null ||
+                queryPurchasesParamsNewBuilderMethod == null ||
+                queryPurchasesParamsBuilderBuildMethod == null ||
+                queryPurchasesParamsBuilderSetProductTypeMethod == null ||
+
+                queryPurchaseHistoryAsyncMethod == null ||
+                queryPurchaseHistoryParamsNewBuilderMethod == null ||
+                queryPurchaseHistoryParamsBuilderBuildMethod == null ||
+                queryPurchaseHistoryParamsBuilderSetProductTypeMethod == null ||
+
+                queryProductDetailsAsyncMethod == null ||
+                queryProductDetailsParamsNewBuilderMethod == null ||
+                queryProductDetailsParamsBuilderBuildMethod == null ||
+                queryProductDetailsParamsBuilderSetProductListMethod == null ||
+                queryProductDetailsParamsProductNewBuilderMethod == null ||
+                queryProductDetailsParamsProductBuilderBuildMethod == null ||
+                queryProductDetailsParamsProductBuilderSetProductIdMethod == null ||
+                queryProductDetailsParamsProductBuilderSetProductTypeMethod == null ||
+
+                billingClientNewBuilderMethod == null ||
+                billingClientBuilderSetListenerMethod == null ||
+                billingClientBuilderEnablePendingPurchasesMethod == null ||
+                billingClientBuilderBuildMethod == null ||
+                billingClientStartConnectionMethod == null
+            ) {
+                Log.w(
+                    TAG,
+                    "Failed to create Google Play billing library wrapper for in-app purchase auto-logging"
+                )
+                return
+            }
 
             // TODO: Create a billing client
 
@@ -183,18 +361,47 @@ private constructor(
                 productDetailsClazz,
                 purchaseHistoryRecordClazz,
                 queryProductDetailsParamsProductClazz,
+
                 queryProductDetailsParamsClazz,
                 queryPurchaseHistoryParamsClazz,
                 queryPurchasesParamsClazz,
+
                 queryProductDetailsParamsBuilderClazz,
                 queryPurchaseHistoryParamsBuilderClazz,
                 queryPurchasesParamsBuilderClazz,
                 queryProductDetailsParamsProductBuilderClazz,
                 billingClientBuilderClazz,
+
                 purchasesUpdatedListenerClazz,
                 billingClientStateListenerClazz,
                 productDetailsResponseListenerClazz,
-                purchasesResponseListenerClazz
+                purchasesResponseListenerClazz,
+                purchaseHistoryResponseListenerClazz,
+
+                queryPurchasesAsyncMethod,
+                queryPurchasesParamsNewBuilderMethod,
+                queryPurchasesParamsBuilderBuildMethod,
+                queryPurchasesParamsBuilderSetProductTypeMethod,
+
+                queryPurchaseHistoryAsyncMethod,
+                queryPurchaseHistoryParamsNewBuilderMethod,
+                queryPurchaseHistoryParamsBuilderBuildMethod,
+                queryPurchaseHistoryParamsBuilderSetProductTypeMethod,
+
+                queryProductDetailsAsyncMethod,
+                queryProductDetailsParamsNewBuilderMethod,
+                queryProductDetailsParamsBuilderBuildMethod,
+                queryProductDetailsParamsBuilderSetProductListMethod,
+                queryProductDetailsParamsProductNewBuilderMethod,
+                queryProductDetailsParamsProductBuilderBuildMethod,
+                queryProductDetailsParamsProductBuilderSetProductIdMethod,
+                queryProductDetailsParamsProductBuilderSetProductTypeMethod,
+
+                billingClientNewBuilderMethod,
+                billingClientBuilderSetListenerMethod,
+                billingClientBuilderEnablePendingPurchasesMethod,
+                billingClientBuilderBuildMethod,
+                billingClientStartConnectionMethod
             )
         }
     }
--- facebook-core/src/test/kotlin/com/facebook/appevents/iap/InAppPurchaseBillingClientWrapperV5PlusTest.kt
+++ facebook-core/src/test/kotlin/com/facebook/appevents/iap/InAppPurchaseBillingClientWrapperV5PlusTest.kt
@@ -12,9 +12,11 @@ import android.content.Context
 import com.facebook.FacebookPowerMockTestCase
 import com.facebook.FacebookSdk
 import com.facebook.appevents.iap.InAppPurchaseUtils.getClass
+import com.facebook.appevents.iap.InAppPurchaseUtils.getMethod
 import org.assertj.core.api.Assertions
 import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.any
 import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
@@ -25,20 +27,57 @@ import org.powermock.core.classloader.annotations.PrepareForTest
 class InAppPurchaseBillingClientWrapperV5PlusTest : FacebookPowerMockTestCase() {
     private val exampleClassName =
         "com.facebook.appevents.iap.InAppPurchaseBillingClientWrapperV5PlusTest"
-    private lateinit var inAppPurchaseBillingClientWrapperV5Plus: InAppPurchaseBillingClientWrapperV5Plus
+    private val exampleMethodName = "setup"
 
     @Before
     override fun setup() {
         super.setup()
-        val mockContext: Context = mock()
         PowerMockito.mockStatic(InAppPurchaseUtils::class.java)
         whenever(getClass(anyOrNull())).thenReturn(Class.forName(exampleClassName))
-        inAppPurchaseBillingClientWrapperV5Plus =
-            InAppPurchaseBillingClientWrapperV5Plus.getOrCreateInstance(mockContext)!!
+        whenever(
+            getMethod(
+                any(),
+                any(),
+                any()
+            )
+        ).thenReturn(
+            Class.forName(exampleClassName)
+                .getMethod(exampleMethodName)
+        )
+
     }
 
     @Test
     fun testHelperClassCanSuccessfullyCreateWrapper() {
+        val mockContext: Context = mock()
+        val inAppPurchaseBillingClientWrapperV5Plus =
+            InAppPurchaseBillingClientWrapperV5Plus.getOrCreateInstance(mockContext)
         Assertions.assertThat(inAppPurchaseBillingClientWrapperV5Plus).isNotNull()
+    }
+
+    @Test
+    fun testCantGetClass() {
+        whenever(getClass(anyOrNull())).thenReturn(null)
+        val mockContext: Context = mock()
+        val inAppPurchaseBillingClientWrapperV5Plus =
+            InAppPurchaseBillingClientWrapperV5Plus.getOrCreateInstance(mockContext)
+        Assertions.assertThat(inAppPurchaseBillingClientWrapperV5Plus).isNull()
+    }
+
+    @Test
+    fun testCantGetMethod() {
+        whenever(
+            getMethod(
+                any(),
+                any(),
+                any()
+            )
+        ).thenReturn(
+            null
+        )
+        val mockContext: Context = mock()
+        val inAppPurchaseBillingClientWrapperV5Plus =
+            InAppPurchaseBillingClientWrapperV5Plus.getOrCreateInstance(mockContext)
+        Assertions.assertThat(inAppPurchaseBillingClientWrapperV5Plus).isNull()
     }
 }
