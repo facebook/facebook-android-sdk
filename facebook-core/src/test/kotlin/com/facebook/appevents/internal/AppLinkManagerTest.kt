d56819b51007700d0ffc770905179162846e107f
Zilin Zhang <kylinchang@meta.com>
2024-09-11 15:01:25 -0700

    Add AppLink support from Intent Extra
    Summary: `al_applink_data` can be put in intent's bundle extra and we also need to support that
    
    Differential Revision: D62169805
    
    fbshipit-source-id: d35a7ab1694a73ce16d34a806505efba48cf3494

--- facebook-core/src/test/kotlin/com/facebook/appevents/internal/AppLinkManagerTest.kt
+++ facebook-core/src/test/kotlin/com/facebook/appevents/internal/AppLinkManagerTest.kt
@@ -13,9 +13,11 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
+import android.os.Bundle
 import com.facebook.FacebookPowerMockTestCase
 import com.facebook.FacebookSdk
 import com.facebook.MockSharedPreference
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.any
@@ -31,20 +33,27 @@ import kotlin.test.assertNull
 class AppLinkManagerTest: FacebookPowerMockTestCase()  {
 
   private lateinit var mockApplicationContext: Context
-  private lateinit var mockSharedPreference: SharedPreferences
   private lateinit var mockActivity: Activity
+
+  companion object {
+    private val mockSharedPreference: SharedPreferences = MockSharedPreference()
+  }
 
   @Before
   fun init() {
     mockActivity = mock()
     mockApplicationContext = mock()
-    mockSharedPreference = MockSharedPreference()
     PowerMockito.mockStatic(FacebookSdk::class.java)
     whenever(FacebookSdk.isInitialized()).thenReturn(true)
     whenever(FacebookSdk.getApplicationId()).thenReturn("123456789")
     whenever(FacebookSdk.getApplicationContext()).thenReturn(mockApplicationContext)
     whenever(mockApplicationContext.getSharedPreferences(any<String>(), any()))
       .thenReturn(mockSharedPreference)
+  }
+
+  @After
+  fun after() {
+    mockSharedPreference.edit().clear()
   }
 
   @Test
@@ -72,10 +81,23 @@ class AppLinkManagerTest: FacebookPowerMockTestCase()  {
   }
 
   @Test
-  fun testHandleNonnullCampaignIDs() {
+  fun testHandleNonnullCampaignIDsFromURL() {
     val intent = Intent()
-    whenever(mockActivity.intent).thenReturn(intent)
     intent.setData(Uri.parse("fb123://test.com?al_applink_data=%7B%22acs_token%22%3A+%22test_token_1234567%22%2C+%22campaign_ids%22%3A+%22test_campaign_1234%22%2C+%22advertiser_id%22%3A+%22test_advertiserid_12345%22%7D"))
+    whenever(mockActivity.intent).thenReturn(intent)
+    AppLinkManager.getInstance()?.handleURL(mockActivity)
+
+    assertEquals("test_campaign_1234", mockSharedPreference.getString("campaign_ids", null))
+  }
+
+  @Test
+  fun testHandleNonnullCampaignIDsFromExtra() {
+    val intent = Intent()
+    val bundle = Bundle()
+    bundle.putString("campaign_ids", "test_campaign_1234")
+    intent.putExtra("al_applink_data", bundle)
+    whenever(mockActivity.intent).thenReturn(intent)
+    intent.setData(Uri.parse("fb123://test.com"))
     AppLinkManager.getInstance()?.handleURL(mockActivity)
 
     assertEquals("test_campaign_1234", mockSharedPreference.getString("campaign_ids", null))
