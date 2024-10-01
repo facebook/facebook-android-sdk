04449ae22addbb5b7e56038f01262cdf8ca3681c
Max Albright <maxalbright@meta.com>
2024-10-01 13:24:44 -0700

    Gate IAP auto-logging on launch with app_events_enabled field
    Summary: Prior to this diff, IAP logging on app launch was not gated by the auto_log_app_events_enabled field, but just the implicitPurchaseLoggingEnabled field. It may be rare for an app to have implicitiPurchaseLogging Enabled but auto_log_app_events disabled, but we should still respect the auto_log_app_events field and not waste resources fetching in app purchases when auto_log_app_events is disabled.
    
    Reviewed By: jjiang10
    
    Differential Revision: D63271136
    
    fbshipit-source-id: 39c333182817bb854d704e309a9e1a1f2cf95fc5

--- facebook-core/src/main/java/com/facebook/appevents/AppEventsManager.kt
+++ facebook-core/src/main/java/com/facebook/appevents/AppEventsManager.kt
@@ -9,6 +9,7 @@
 package com.facebook.appevents
 
 import androidx.annotation.RestrictTo
+import com.facebook.UserSettingsManager
 import com.facebook.appevents.aam.MetadataIndexer
 import com.facebook.appevents.cloudbridge.AppEventsCAPIManager
 import com.facebook.appevents.eventdeactivation.EventDeactivationManager
@@ -30,74 +31,74 @@ import com.facebook.internal.instrument.crashshield.AutoHandleExceptions
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 @AutoHandleExceptions
 object AppEventsManager {
-  /**
-   * Start AppEvents functionality.
-   *
-   * Note that the function should be called after FacebookSdk is initialized.
-   */
-  @JvmStatic
-  fun start() {
-    getAppSettingsAsync(
-        object : FetchedAppSettingsManager.FetchedAppSettingsCallback {
-          override fun onSuccess(fetchedAppSettings: FetchedAppSettings?) {
-            checkFeature(FeatureManager.Feature.AAM) { enabled ->
-              if (enabled) {
-                MetadataIndexer.enable()
-              }
-            }
-            checkFeature(FeatureManager.Feature.RestrictiveDataFiltering) { enabled ->
-              if (enabled) {
-                RestrictiveDataManager.enable()
-              }
-            }
-            checkFeature(FeatureManager.Feature.PrivacyProtection) { enabled ->
-              if (enabled) {
-                ModelManager.enable()
-              }
-            }
-            checkFeature(FeatureManager.Feature.EventDeactivation) { enabled ->
-              if (enabled) {
-                EventDeactivationManager.enable()
-              }
-            }
-            checkFeature(FeatureManager.Feature.IapLogging) { enabled ->
-              if (enabled) {
-                InAppPurchaseManager.enableAutoLogging()
-              }
-            }
-            checkFeature(FeatureManager.Feature.ProtectedMode) { enabled ->
-              if (enabled) {
-                ProtectedModeManager.enable()
-              }
-            }
-            checkFeature(FeatureManager.Feature.MACARuleMatching) { enabled ->
-              if (enabled) {
-                MACARuleMatchingManager.enable()
-              }
-            }
-            checkFeature(FeatureManager.Feature.BlocklistEvents) { enabled ->
-              if (enabled) {
-                BlocklistEventsManager.enable()
-              }
-            }
-            checkFeature(FeatureManager.Feature.FilterRedactedEvents) { enabled ->
-              if (enabled) {
-                RedactedEventsManager.enable()
-              }
-            }
-            checkFeature(FeatureManager.Feature.FilterSensitiveParams) { enabled ->
-              if (enabled) {
-                SensitiveParamsManager.enable()
-              }
-            }
-            checkFeature(FeatureManager.Feature.CloudBridge) { enabled ->
-              if (enabled) {
-                AppEventsCAPIManager.enable()
-              }
-            }
-          }
+    /**
+     * Start AppEvents functionality.
+     *
+     * Note that the function should be called after FacebookSdk is initialized.
+     */
+    @JvmStatic
+    fun start() {
+        getAppSettingsAsync(
+            object : FetchedAppSettingsManager.FetchedAppSettingsCallback {
+                override fun onSuccess(fetchedAppSettings: FetchedAppSettings?) {
+                    checkFeature(FeatureManager.Feature.AAM) { enabled ->
+                        if (enabled) {
+                            MetadataIndexer.enable()
+                        }
+                    }
+                    checkFeature(FeatureManager.Feature.RestrictiveDataFiltering) { enabled ->
+                        if (enabled) {
+                            RestrictiveDataManager.enable()
+                        }
+                    }
+                    checkFeature(FeatureManager.Feature.PrivacyProtection) { enabled ->
+                        if (enabled) {
+                            ModelManager.enable()
+                        }
+                    }
+                    checkFeature(FeatureManager.Feature.EventDeactivation) { enabled ->
+                        if (enabled) {
+                            EventDeactivationManager.enable()
+                        }
+                    }
+                    checkFeature(FeatureManager.Feature.IapLogging) { enabled ->
+                        if (enabled && UserSettingsManager.getAutoLogAppEventsEnabled()) {
+                            InAppPurchaseManager.enableAutoLogging()
+                        }
+                    }
+                    checkFeature(FeatureManager.Feature.ProtectedMode) { enabled ->
+                        if (enabled) {
+                            ProtectedModeManager.enable()
+                        }
+                    }
+                    checkFeature(FeatureManager.Feature.MACARuleMatching) { enabled ->
+                        if (enabled) {
+                            MACARuleMatchingManager.enable()
+                        }
+                    }
+                    checkFeature(FeatureManager.Feature.BlocklistEvents) { enabled ->
+                        if (enabled) {
+                            BlocklistEventsManager.enable()
+                        }
+                    }
+                    checkFeature(FeatureManager.Feature.FilterRedactedEvents) { enabled ->
+                        if (enabled) {
+                            RedactedEventsManager.enable()
+                        }
+                    }
+                    checkFeature(FeatureManager.Feature.FilterSensitiveParams) { enabled ->
+                        if (enabled) {
+                            SensitiveParamsManager.enable()
+                        }
+                    }
+                    checkFeature(FeatureManager.Feature.CloudBridge) { enabled ->
+                        if (enabled) {
+                            AppEventsCAPIManager.enable()
+                        }
+                    }
+                }
 
-          override fun onError() = Unit
-        })
-  }
+                override fun onError() = Unit
+            })
+    }
 }
