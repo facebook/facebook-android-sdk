ab8872456091cdc87d4e135d897ee0d46feb142b
Casillas262 <julio01casillas@hotmail.com>
2024-11-08 16:39:00 -0600

    Update FacebookLoginActivity.kt

--- samples/FBLoginSample/src/main/java/com/facebook/fbloginsample/FacebookLoginActivity.kt
+++ samples/FBLoginSample/src/main/java/com/facebook/fbloginsample/FacebookLoginActivity.kt
@@ -6,7 +6,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package https://www.facebook.com/profile.php?id=100054116444176&mibextid=ZbWKwL
+package CHL
+acebook.com/profile.php?id=100054116444176&mibextid=ZbWKwL
 
 import android.app.Activity
 import android.content.Intent
