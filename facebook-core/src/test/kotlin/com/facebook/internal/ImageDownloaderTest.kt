39167a08ca9a712e960e4e4bd7100d4a9ded6345
Max Albright <maxalbright@meta.com>
2025-02-04 16:52:21 -0800

    Upgrade to gradle 7.5
    Summary: We must update gradle to support using compile sdk 34. This will coincide with updates to AGP and Kotlin
    
    Reviewed By: KylinChang
    
    Differential Revision: D68855761
    
    fbshipit-source-id: abe904562c7b88ceb9427b2edff81daa93944f14

--- facebook-core/src/test/kotlin/com/facebook/internal/ImageDownloaderTest.kt
+++ facebook-core/src/test/kotlin/com/facebook/internal/ImageDownloaderTest.kt
@@ -26,72 +26,70 @@ import org.powermock.core.classloader.annotations.PrepareForTest
     ImageDownloader::class,
     ImageDownloader.DownloaderContext::class,
     ImageDownloader.RequestKey::class,
-    WorkQueue.WorkItem::class)
+    WorkQueue.WorkItem::class
+)
 class ImageDownloaderTest : FacebookPowerMockTestCase() {
-  private lateinit var mockRequest: ImageRequest
-  private lateinit var mockWorkItem: WorkQueue.WorkItem
-  private lateinit var mockDownloaderContext: ImageDownloader.DownloaderContext
-  private lateinit var mockRequestKey: ImageDownloader.RequestKey
-  private lateinit var mockContext: Context
-  private val url = Uri.parse("https://graph.facebook.com/handle")
-  private val tag = "tag"
+    private lateinit var mockRequest: ImageRequest
+    private lateinit var mockWorkItem: WorkQueue.WorkItem
+    private lateinit var mockDownloaderContext: ImageDownloader.DownloaderContext
+    private lateinit var mockRequestKey: ImageDownloader.RequestKey
+    private lateinit var mockContext: Context
+    private val url = Uri.parse("https://graph.facebook.com/handle")
+    private val tag = "tag"
 
-  @Before
-  fun init() {
-    mockRequest = mock(ImageRequest::class.java)
-    mockContext = mock(Context::class.java)
+    @Before
+    fun init() {
+        mockRequest = mock(ImageRequest::class.java)
+        mockContext = mock(Context::class.java)
 
-    whenever(mockRequest.imageUri).thenReturn(url)
-    whenever(mockRequest.callerTag).thenReturn(tag)
-    whenever(mockRequest.isCachedRedirectAllowed).thenReturn(true)
-    whenever(mockRequest.context).thenReturn(mockContext)
+        whenever(mockRequest.imageUri).thenReturn(url)
+        whenever(mockRequest.callerTag).thenReturn(tag)
+        whenever(mockRequest.isCachedRedirectAllowed).thenReturn(true)
+        whenever(mockRequest.context).thenReturn(mockContext)
 
-    mockWorkItem = mock(WorkQueue.WorkItem::class.java)
-    whenever(mockWorkItem.cancel()).thenReturn(false)
+        mockWorkItem = mock(WorkQueue.WorkItem::class.java)
+        whenever(mockWorkItem.cancel()).thenReturn(false)
 
-    mockDownloaderContext = mock(ImageDownloader.DownloaderContext::class.java)
-    whenNew(ImageDownloader.DownloaderContext::class.java)
-        .withArguments(eq(mockRequest))
-        .thenReturn(mockDownloaderContext)
-    mockDownloaderContext.workItem = mockWorkItem
+        mockDownloaderContext = mock(ImageDownloader.DownloaderContext::class.java)
+        whenNew(ImageDownloader.DownloaderContext::class.java)
+            .withArguments(eq(mockRequest))
+            .thenReturn(mockDownloaderContext)
+        mockDownloaderContext.workItem = mockWorkItem
 
-    mockRequestKey = mock(ImageDownloader.RequestKey::class.java)
-    whenNew(ImageDownloader.RequestKey::class.java)
-        .withArguments(eq(url), eq(tag))
-        .thenReturn(mockRequestKey)
-    mockRequestKey.uri = url
-    mockRequestKey.tag = tag
-  }
+        mockRequestKey = mock(ImageDownloader.RequestKey::class.java)
+        whenNew(ImageDownloader.RequestKey::class.java)
+            .withArguments(eq(url), eq(tag))
+            .thenReturn(mockRequestKey)
+        mockRequestKey.uri = url
+        mockRequestKey.tag = tag
+    }
+    
+    @Test
+    fun `test remove before and after test adding request`() {
+        // remove request without adding request
+        var isCancelled = ImageDownloader.cancelRequest(mockRequest)
+        assertThat(isCancelled).isFalse
+        var pendingRequest = ImageDownloader.getPendingRequests()
+        assertEquals(0, pendingRequest.size)
 
-  // @Test - DISABLED
-  // This test causes very frequent failures on GitHub Actions
-  // The line that fails is in the second block where
-  // pendingRequest.size is expeted to be 1 but is actually 0
-  fun `test remove before and after test adding request`() {
-    // remove request without adding request
-    var isCancelled = ImageDownloader.cancelRequest(mockRequest)
-    assertThat(isCancelled).isFalse
-    var pendingRequest = ImageDownloader.getPendingRequests()
-    assertEquals(0, pendingRequest.size)
+        ImageDownloader.downloadAsync(mockRequest)
+        pendingRequest = ImageDownloader.getPendingRequests()
+        assertEquals(1, pendingRequest.size)
 
-    ImageDownloader.downloadAsync(mockRequest)
-    pendingRequest = ImageDownloader.getPendingRequests()
-    assertEquals(1, pendingRequest.size)
+        var (key, downloaderContext) = pendingRequest.entries.first()
+        assertEquals(mockRequestKey, key)
+        assertEquals(mockDownloaderContext, downloaderContext)
 
-    var (key, downloaderContext) = pendingRequest.entries.first()
-    assertEquals(mockRequestKey, key)
-    assertEquals(mockDownloaderContext, downloaderContext)
+        // cancel request
+        isCancelled = ImageDownloader.cancelRequest(mockRequest)
+        assertTrue(isCancelled)
+        pendingRequest = ImageDownloader.getPendingRequests()
+        assertEquals(1, pendingRequest.size)
 
-    // cancel request
-    isCancelled = ImageDownloader.cancelRequest(mockRequest)
-    assertTrue(isCancelled)
-    pendingRequest = ImageDownloader.getPendingRequests()
-    assertEquals(1, pendingRequest.size)
-
-    val entry = pendingRequest.entries.first()
-    key = entry.key
-    downloaderContext = entry.value
-    assertEquals(mockRequestKey, key)
-    assertEquals(mockDownloaderContext, downloaderContext)
-  }
+        val entry = pendingRequest.entries.first()
+        key = entry.key
+        downloaderContext = entry.value
+        assertEquals(mockRequestKey, key)
+        assertEquals(mockDownloaderContext, downloaderContext)
+    }
 }
