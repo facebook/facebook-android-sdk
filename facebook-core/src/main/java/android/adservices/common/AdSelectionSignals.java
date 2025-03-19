b972f69d1a13f87de392fdcb03ae855d940a99ae
Shen Guo <shenguo@meta.com>
2025-03-19 13:38:40 -0700

    Removing stub classes
    Summary: These stub classes was previously added to make the code compile, which is not useful anymore since the SDK team upgrading the compile SDK level to be 34.
    
    Reviewed By: youerkang
    
    Differential Revision: D71489685
    
    fbshipit-source-id: 78b71258e7a87e03702da60abfc55037875a5b7e

--- facebook-core/src/main/java/android/adservices/common/AdSelectionSignals.java
+++
@@ -1,9 +0,0 @@
-package android.adservices.common;
-
-import androidx.annotation.NonNull;
-
-public class AdSelectionSignals {
-    public static AdSelectionSignals fromString(@NonNull String source) {
-        throw new RuntimeException("Stub!");
-    }
-}
